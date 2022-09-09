1961b0ef9e558addfd81b38866e4633bd51dd74c
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-09 12:08:57 -0700

    Time: 101 ms (41.09%), Space: 42.7 MB (79.99%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+   for(let start = 0, end = numbers.length-1; start < end;){
+        let sum = numbers[start] + numbers[end]
+        if(sum === target)return [++start, ++end]
+        else sum > target ? end-- : start++
+    }
+};
