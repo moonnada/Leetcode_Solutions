3b7e22a8bf16334532b5276c10460fe1346aca3e
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-09 15:21:05 -0700

    Time: 261 ms (33.36%), Space: 51.6 MB (97.44%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,33 +3,45 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+   /*
+   arr = [-1,0,1,2,-1,-4]
+   1. sort the input arr. [-4,-1,-1,0,1,2]
+   2. init a target val as 0
+   3. traverse the sorted arr from 0 to length-2. the reason length-2 is i am going to add the right pointer later
+   4. check if cur element > target, then break the loop. if cur element is a postive num, can't get the sum is 0
+   5. check if there is a duplicate element constantly, then continue. the reason is to escape getting a same format 
+   6. init the mid and right value
+   7. while(mid < right), init sum val = n[i] + n[j] + n[k]
+   8. if sum = target, then put the element into output arr
+   9.   also check there is same element constantly or not on mid place and right place both.
+   10. else sum > target, right--, else mid++
+   
+   */
+    
     let ans = [];
-    if(nums.length < 3) return ans;
-    
-    nums.sort((a,b) => a-b);
-    
     let target = 0;
+    nums.sort((a,b) => a-b);
     for(let i=0; i<nums.length-2; i++){
-        if(nums[i] > target ) break;
-        
-        if(i>0 && nums[i] === nums[i-1]) continue;
+        if(nums[i] > target) break;
+        if(i> 0 && nums[i] === nums[i-1]) continue;
         
         let j = i+1;
         let k = nums.length-1;
         while(j<k){
             let sum = nums[i] + nums[j] + nums[k];
-            
             if(sum === target){
-                ans.push([nums[i], nums[j], nums[k]])
+                ans.push([nums[i], nums[j], nums[k]]);
                 
-                while(nums[j] === nums[j+1]) j++;
+                while(nums[j]=== nums[j+1]) j++;
                 while(nums[k] === nums[k-1]) k--;
                 
                 j++;
                 k--;
             }
-            else sum > target ? k-- : j++
+            else {
+                sum > target ? k-- : j++;
+            }
         }
     }
-    return ans;
-};
+    return ans
+}
