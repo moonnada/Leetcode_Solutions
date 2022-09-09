d35f386bdd4f9b3952375e992dc80347d7030087
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-09 15:30:04 -0700

    Time: 237 ms (45.26%), Space: 53.1 MB (38.90%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -7,13 +7,14 @@ var threeSum = function(nums) {
    arr = [-1,0,1,2,-1,-4]
    1. sort the input arr. [-4,-1,-1,0,1,2]
    2. init a target val as 0
-   3. traverse the sorted arr from 0 to length-2. the reason length-2 is i am going to add the right pointer later
+   3. traverse the sorted arr from 0 to length-2. the reason length-2 is I am going to add the right pointer as nums.length -1 later
    4. check if cur element > target, then break the loop. if cur element is a postive num, can't get the sum is 0
    5. check if there is a duplicate element constantly, then continue. the reason is to escape getting a same format 
    6. init the mid and right value
    7. while(mid < right), init sum val = n[i] + n[j] + n[k]
    8. if sum = target, then put the element into output arr
    9.   also check there is same element constantly or not on mid place and right place both.
+   10. mid is incremented and right is decremented
    10. else sum > target, right--, else mid++
    
    */
