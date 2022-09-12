53b1fb7011ed6536e53937eca2f68b6484405d6c
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-11 17:35:14 -0700

    Time: 106 ms (99.50%), Space: 52 MB (96.55%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,6 +3,8 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    //time : o(n*klgk) where n is the length of input array and k is the maximum length of a string in input array
+    //space : o(n)
     let map = new Map();
     for(let str of strs){
         let sorted = str.split("").sort().join("");
