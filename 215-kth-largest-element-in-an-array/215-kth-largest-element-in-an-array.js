14551dda9f1b1724c34dd9c7e0494cf2cb6dc594
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-13 21:59:11 -0700

    Time: 123 ms (92.49%), Space: 48.7 MB (90.70%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -4,44 +4,38 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    let finalIndex = nums.length -k;
+    //quick sort
+    let finalIndex = nums.length - k;
     let left = 0;
     let right = nums.length-1;
     
-    while(left<= right){
-        //random num between left and right
-        const pivot = Math.floor(Math.random() * (right - left + 1)) + left;
-        
-        //the final position of the pivot in a sorted arr
+    while(left <= right){
+        const pivot = Math.floor(Math.random() * (right-left + 1)) + left;
         const pivotIndex = pivotHelper(pivot, left, right);
-        
-        //the target num is in its correct position, thus reutn
-        if(pivotIndex === finalIndex) return nums[finalIndex];
-        
-        //if pivotIndex is smaller than finalIndex, only look the right half
-        if(pivotIndex < finalIndex) left = pivotIndex+1;
-        else right = pivotIndex-1;
+        if(pivotIndex === finalIndex) return nums[finalIndex]
+        if(pivotIndex < finalIndex) left = pivotIndex + 1;
+        else right = pivotIndex -1;
     }
     
     function pivotHelper(pivot, start, end){
-        //move the pivot to the end
         swap(pivot, end);
+        let i = start
+        let j = start
         
-        let i = start;
-        let j = start;
         while(j<end){
             if(nums[j] <= nums[end]){
-                swap(i,j)
-                i++;
+                swap(i,j);
+                i++
             }
-            j++;
+            j++
         }
         
-        swap(i,end);
-        return i;
+        swap(i,end)
+        return i
     }
     
     function swap(i,j){
-        [nums[i], nums[j]] = [nums[j], nums[i]]
+        [nums[i], nums[j] ] = [nums[j] , nums[i]]
     }
-};
+    
+}
