c1d149d5f5c60738121e8b96f4afc5f6189151e9
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-20 23:36:26 -0700

    Time: 226 ms (58.74%), Space: 57.3 MB (77.03%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,17 +3,24 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
+    /*
+    Method: hashset, time: o(n), space: o(n)
+    1. save input array as a set
+    2. count consecutive numbers within the set by keep adding 1 and record max
+    3. only check numbers that is the smallest in a consecutive(!set.has(n-1)).
+    */
+    
     let set = new Set(nums);
+    let cnt = 0;
     let max = 0;
     for(let n of set){
         if(!set.has(n-1)){
-            let cnt = 0;
+            cnt = 0;
             while(set.has(n)){
-                n += 1;
+                n++;
                 cnt++;
+                max = Math.max(cnt, max);
             }
-            max = Math.max(cnt, max)
         }
-    }
-    return max
+    } return max    
 };
