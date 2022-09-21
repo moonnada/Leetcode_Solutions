fcd30cd7731f351c7e2364f158364593dc5f97a2
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-20 23:03:43 -0700

    Time: 137 ms (37.17%), Space: 46.9 MB (92.15%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.js
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.js
@@ -7,6 +7,7 @@
 var encode = function(strs) {
     if(!strs.length) return null
     return strs.join("</>");
+    
 };
 
 /**
@@ -18,6 +19,7 @@ var encode = function(strs) {
 var decode = function(s) {
     if(s === null) return []
     return s.split("</>")
+    
 };
 
 /**
