979ee1c07bcc934d35db722133ef9559a7c4c691
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-21 23:31:23 -0700

    Time: 125 ms (63.57%), Space: 42.6 MB (69.24%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -4,16 +4,19 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    let left = 0, right = 0, max = 0, mostFreq = 0;
-    let freqHash = {};
-    for(let right = 0; right<s.length; right++){
-        freqHash[s[right]] = freqHash[s[right]] + 1 || 1;
-        mostFreq = Math.max(mostFreq, freqHash[s[right]])
-        while(right - left +1 - mostFreq > k){
-            freqHash[s[left]]--;
+    /*
+    using slide window. left and right
+    */
+    let left = 0, right = 0, max = 0, freq = 0;
+    let arr = [];
+    for(right = 0; right<s.length; right++){
+        arr[s[right]] = arr[s[right]]+1 || 1;
+        freq = Math.max(freq , arr[s[right]]);
+        while(right-left+1 - freq > k){
+            arr[s[left]]--;
             left++;
         }
-        max = Math.max(max, right - left + 1)
+        max = Math.max(max, right-left+1)
     }
-return max
+    return max
 };
