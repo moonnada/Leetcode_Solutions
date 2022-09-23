d9b8b090671ee3a4af0ca1a952f94ac75edea0bd
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-22 18:58:21 -0700

    Time: 108 ms (83.72%), Space: 45.3 MB (59.75%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.js
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -4,45 +4,33 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-
-	if (s1.length > s2.length) return false;
-	let neededChar = {}; //To Store the frequency/count of required string s1
-	for (let i = 0; i < s1.length; i++) {
-		neededChar[s1[i]] = (neededChar[s1[i]] || 0) + 1;
-	}
-
-	let left = 0, //left pointer/index of the sliding window
-		right = 0, //right pointer/index of the sliding window
-		requiredLength = s1.length //length of the substring required in s2
-
-	// Now iterate until the right index of window is lesser than length of s2
-	while(right < s2.length) {
-		// If we found s2 character in s1 i.e in neededChar then we decrease requiredLength
-		if (neededChar[s2[right]] > 0) requiredLength--;
-		// Since we have encountered new char i.e s2[right] we decrease it's 
-		// count in neededChar even if it is not present in neededChar because we only care about neededChars
-        console.log(neededChar[s2[right]])
-		neededChar[s2[right]]--;
-		right++ //window is incremented by 1 step
-
-		// Now if our requiredLength becomes 0 it means we have found a match of the s2 substring
-		// So we return true
-		if (requiredLength === 0) return true;
-
-		// If our window length is equal to s1 length (length of string to search in s2)
-		// then we have to remove left element of window i.e left++ and add new element from right 
-		// will be added in next iteration
-		if (right - left === s1.length) {
-			// if the left element we are removing was a required character then we increase requiredLength
-			// because that element will no longer be the part of sliding window
-            console.log(neededChar[s2[left]])
-			if (neededChar[s2[left]] >= 0) requiredLength++;
-			// We will also increase the count of left element removed from window
-			neededChar[s2[left]]++;
-			// And finally we will decrease the window size by 1 from left i.e left++
-			left++
-		}
-	}
-	// If match was not found we return false
-	return false;
+    if(s1.length > s2.length) return false
+    
+    let needed = {};
+    for(let i=0; i<s1.length; i++){
+        needed[s1[i]] = (needed[s1[i]] || 0) +1; 
+    }
+    
+    let left =0, right = 0, requiredLength = s1.length;
+    
+    while(right < s2.length){
+        if(needed[s2[right]] > 0 ) {
+            requiredLength--;
+        }
+        needed[s2[right]]--;
+        right++
+        
+        if(requiredLength === 0) return true;
+        
+        if(right - left === s1.length){
+            if(needed[s2[left]] >=0) {
+                requiredLength++;
+            }
+            needed[s2[left]]++;
+            left++
+        }
+        
+    }
+    return false
+    
 };
