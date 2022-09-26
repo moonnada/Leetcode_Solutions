981421a7c24395da241fe3fe8d475608a7b6de25
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-26 12:03:12 -0700

    Time: 94 ms (55.87%), Space: 42.6 MB (72.11%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.js
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -6,12 +6,16 @@ var generateParenthesis = function(n) {
     //backtracking and dfs
     let ans = [];
     function dfs(str, open, close){
+        //backtracking case: number of ( is less then number of )
         if(open < close) return;
         
+        //there are n number of open and close case
         if(open === n && close === n) {
             ans.push(str);
             return;
         }
+        
+        //dfs traversal
         if(open <n) dfs(str+"(", open+1, close);
         if(close < n) dfs(str+")", open, close+1);
     }
