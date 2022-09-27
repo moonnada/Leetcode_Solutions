fdcd38000e3a2fb733e4006046e418f99eeecf72
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-27 02:31:51 -0700

    Time: 275 ms (94.81%), Space: 67.7 MB (95.13%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function(target, position, speed) {
+    let cars = [];
+    for(let i=0; i<position.length; i++){
+        cars.push({
+            pos: position[i],
+            speed: speed[i]
+        })
+    }
+    
+    cars.sort((a,b) => b.pos - a.pos);
+    
+    let stack = [];
+    
+    for(let i=0; i<cars.length; i++){
+        const car = cars[i];
+        const time = (target - car.pos) / car.speed;
+        
+        if(stack.length === 0 || time > stack[stack.length-1]){
+            stack.push(time)
+        }
+    }
+    return stack.length
+};
