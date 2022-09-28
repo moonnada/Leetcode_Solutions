79bc589e0e1d7e4db11c63393ea6cf4d9c4e9579
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-27 23:45:02 -0700

    Time: 82 ms (69.92%), Space: 42.3 MB (34.84%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -5,15 +5,28 @@
  */
 var searchMatrix = function(matrix, target) {
     //diagonal search. Time : o(m+n)
-    if(!matrix.length || !matrix[0].length) return false;
+//     if(!matrix.length || !matrix[0].length) return false;
     
-    let row = 0;
-    let col = matrix[0].length-1;
+//     let row = 0;
+//     let col = matrix[0].length-1;
     
-    while(col >= 0 && row <= matrix.length-1){
-        if(matrix[row][col] === target) return true;
-       else if(matrix[row][col] < target) row++;
-        else if(matrix[row][col] > target) col--;
+//     while(col >= 0 && row <= matrix.length-1){
+//         if(matrix[row][col] === target) return true;
+//        else if(matrix[row][col] < target) row++;
+//         else if(matrix[row][col] > target) col--;
+//     }
+//     return false
+    
+    //binary search. Time : O(mlgn)
+    for(let i=0; i<matrix.length; i++){
+        let start = 0, end = matrix[0].length-1;
+        
+        while(start <= end){
+            let mid = Math.floor((start + end) / 2);
+            if(matrix[i][mid] === target) return true;
+            else if(matrix[i][mid] > target) end = mid-1;
+            else if(matrix[i][mid] < target) start = mid+1
+        }
     }
     return false
 };
