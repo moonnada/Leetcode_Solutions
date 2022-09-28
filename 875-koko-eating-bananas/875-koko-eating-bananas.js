6fd4084f8d5b6de4244a1ec3a4091e295708d7a9
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-28 00:54:38 -0700

    Time: 99 ms (81.24%), Space: 45.9 MB (14.07%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.js
+++ 875-koko-eating-bananas/875-koko-eating-bananas.js
@@ -7,7 +7,7 @@ var minEatingSpeed = function(piles, h) {
     /*
     [30,11,23,4,20] , h = 6.
     
-    Binary
+    Binary.  //time : O(nlgm). n be the length of the input array, m be the maximum number of bananas
     1. init start and end val. ex) start = 1, end = 30
     2. while start <= end
     2-1) get mid val between start and end. (1+30)/2 = 15
@@ -29,18 +29,4 @@ var minEatingSpeed = function(piles, h) {
         else end = mid -1
     }
     return start
-    //time : O(nlgm). n be the length of the input array, m be the maximum number of bananas
-//     let start = 1;
-//     let end = Math.max(...piles);
-    
-//     while(start <= end){
-//         let mid = Math.floor((start + end) / 2);
-//         let hrs = 0;
-//         for(let pile of piles){
-//             hrs += Math.ceil(pile/mid);
-//         }
-//         if(hrs > h) start = mid + 1;
-//         else end = mid -1;
-//     }
-//     return start
 };
