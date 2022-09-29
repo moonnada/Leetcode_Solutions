22984dc24668e4131f080da7896eca8b358c48fe
moonnada <53587841+moonnada@users.noreply.github.com>
2022-09-29 00:06:21 -0700

    Time: 88 ms (59.21%), Space: 41.9 MB (80.76%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    /*
+    binary
+    1. init start and end val
+    2. while start <= end
+    2-1) init a mid val
+    2-2) if arr[mid] > arr[mid-1], end = mid-1
+    2-3) else start = mid+1
+    */
+    let start = 0, end = nums.length-1;
+   
+    while(start < end){
+        let mid = Math.floor((start + end) / 2);
+        if(nums[mid] > nums[end]) start = mid +1;
+        else end = mid
+    }
+    return nums[start]
+};
