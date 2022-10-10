c9233d3b4788e87d6b278a954d6a40f6666a7dc1
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-09 22:26:15 -0700

    Time: 166 ms (78.47%), Space: 48.3 MB (96.30%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -5,23 +5,37 @@
  */
 var findKthLargest = function(nums, k) {
     //quicksort
+    //the final position of the kth largest number in a sorted arr
     let finalIndex = nums.length-k;
     let left = 0;
     let right = nums.length-1;
     
     while(left <= right){
+        //random num between left and right
         let pivot = Math.floor(Math.random() * (right-left+1))+left;
+        
+        //the final position of the pivot in a sorted arr
         let pivotIndex = pivotHelper(pivot, left, right);
+        
+        //the target num is on the correct position, thus return
         if(pivotIndex === finalIndex) return nums[finalIndex];
+        
+        //if pivotIdx < finalIndex, only look on the right half
         if(pivotIndex < finalIndex) left = pivotIndex+1;
+        
+        //else, only look on the left half
         else right = pivotIndex-1;
     }
     
     function pivotHelper(pivot, start, end){
+        //move the pivot to the end
         swap(pivot, end);
+        
         let i= start;
         let j = start;
-        while(j<end){
+        
+        //move smaller nums to the beginning of the arr
+        while( j<end ){
             if(nums[j] <= nums[end]){
                 swap(i,j);
                 i++;
