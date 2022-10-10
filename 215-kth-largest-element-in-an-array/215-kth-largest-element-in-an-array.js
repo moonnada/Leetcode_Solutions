7c5deb8e04861c7a844441ee4500c94dd12358f3
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-09 21:51:00 -0700

    Time: 123 ms (94.19%), Space: 50 MB (68.09%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -4,38 +4,35 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    //quick sort
-    let finalIndex = nums.length - k;
+    //quicksort
+    let finalIndex = nums.length-k;
     let left = 0;
     let right = nums.length-1;
     
     while(left <= right){
-        const pivot = Math.floor(Math.random() * (right-left + 1)) + left;
-        const pivotIndex = pivotHelper(pivot, left, right);
-        if(pivotIndex === finalIndex) return nums[finalIndex]
-        if(pivotIndex < finalIndex) left = pivotIndex + 1;
-        else right = pivotIndex -1;
+        let pivot = Math.floor(Math.random() * (right-left+1))+ left;
+        let pivotIndex = pivotHelper(pivot, left, right);
+        if(pivotIndex === finalIndex) return nums[finalIndex];
+        if(pivotIndex < finalIndex) left = pivotIndex+1;
+        else right = pivotIndex-1;
     }
     
     function pivotHelper(pivot, start, end){
         swap(pivot, end);
-        let i = start
-        let j = start
-        
+        let i= start;
+        let j = start;
         while(j<end){
             if(nums[j] <= nums[end]){
                 swap(i,j);
-                i++
+                i++;
             }
-            j++
+            j++;
         }
-        
-        swap(i,end)
-        return i
+        swap(i,end);
+        return i;
     }
     
     function swap(i,j){
-        [nums[i], nums[j] ] = [nums[j] , nums[i]]
+        [nums[i] , nums[j] ] = [nums[j] , nums[i]]
     }
-    
-}
+};
