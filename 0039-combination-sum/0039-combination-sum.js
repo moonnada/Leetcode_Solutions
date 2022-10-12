670fd4a72a783d6e270505e053749bcdb4f5edba
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-11 23:38:26 -0700

    Time: 149 ms (34.16%), Space: 44.8 MB (95.70%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    let buffer = [];
+    let ans = [];
+    backtracking(0, target);
+    return ans;
+    
+    function backtracking(startIndex, target){
+        if(target === 0) return ans.push(buffer.slice());
+        if(target < 0) return;
+        if(startIndex === candidates.length) return;
+        buffer.push(candidates[startIndex]);
+        backtracking(startIndex, target - candidates[startIndex]);
+        buffer.pop();
+        backtracking(startIndex+1, target)
+    }
+};
