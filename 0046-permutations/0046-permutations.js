5f928c670bc274fc353dc2d7d196cd97ae692e41
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-12 21:35:08 -0700

    Time: 79 ms (93.59%), Space: 45.5 MB (22.04%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -4,14 +4,15 @@
  */
 var permute = function(nums) {
     let ans = [];
-    backtracking([], nums);
-    return ans
+    backtracking([],nums);
+    return ans;
     
-    function backtracking(cur, rem){
-        if(rem.length === 0) ans.push([...cur]);
+   function backtracking(cur, rem){
+        if(!rem.length) return ans.push(cur);
         for(let i=0; i<rem.length; i++){
             let newCur = [...cur];
             let newRem = [...rem];
+            
             newCur.push(newRem[i]);
             newRem.splice(i,1);
             backtracking(newCur, newRem)
