cee169cdf4e9237d143de6fa8543b075bae64254
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-13 21:54:11 -0700

    Time: 94 ms (75.30%), Space: 44.7 MB (50.81%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -4,18 +4,19 @@
  */
 var subsetsWithDup = function(nums) {
     nums = nums.sort((a,b) => a-b);
-    let ans = [];
-    backtracking(nums, [], 0);
+    let ans = [[]];
+    backtracking(0,[]);
     return ans;
     
-    function backtracking(nums, subSets, idx){
-        ans.push([...subSets]);
-        for(let i=idx; i<nums.length; i++){
-            if(i>idx && nums[i-1] === nums[i]) continue;
-            let newCur = nums[i];
-            subSets.push(newCur);
-            backtracking(nums, subSets, i+1);
-            subSets.pop();
+    function backtracking(first, cur){
+        for(let i=first; i<nums.length; i++){
+            if(i> first &&nums[i-1] === nums[i]) continue;
+            cur.push(nums[i]);
+            ans.push([...cur])
+            backtracking(i+1, cur);
+            cur.pop()
         }
     }
+    
+   
 };
