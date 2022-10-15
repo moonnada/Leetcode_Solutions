db97385c6da04d7c756195202e2ade173e4e2cad
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-14 23:20:46 -0700

    Time: 867 ms (52.75%), Space: 42.6 MB (77.54%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -15,31 +15,33 @@ var exist = function(board, word) {
     3.4) visit recursively horizontal and vertical ways
     */
     
-    let row = board.length;
+    let row = board.length; 
     let col = board[0].length;
-    
     for(let i=0; i<row; i++){
         for(let j=0; j<col; j++){
-            if(board[i][j] === word.charAt(0) && dfs(i,j, 0, word, board)) return true;
+            if(word.charAt(0) === board[i][j] && dfs(i,j, 0, word, board)) return true;
         }
     }
+    
     return false;
     
     function dfs(i,j, idx, word, board){
         if(idx === word.length) return true;
-        if(i < 0 || i >= board.length || j <0 || j>= board[i].length || word.charAt(idx) !== board[i][j] ) return false;
+        if(i<0 || i>= board.length || j < 0 || j>= board[i].length || word.charAt(idx) !== board[i][j]) return false;
         
+        //choose
         let tmp = board[i][j];
         board[i][j] = "*";
         
-        if( dfs(i+1,j,idx+1,word,board) ||
-            dfs(i,j+1, idx+1, word, board) ||
+        //explore
+        if( dfs(i+1, j, idx+1, word,board) ||
             dfs(i-1, j, idx+1, word, board) ||
+            dfs(i, j+1, idx+1, word, board) ||
             dfs(i, j-1, idx+1, word, board)
           ) return true;
         
+        //unchoose
         board[i][j] = tmp;
-        
         return false
     }
 };
