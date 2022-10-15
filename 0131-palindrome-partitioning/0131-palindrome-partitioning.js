93ed60e4e4f6d7855d7e35c13c1b6d065794e7ef
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-15 11:09:44 -0700

    Time: 549 ms (17.38%), Space: 80.6 MB (57.13%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -7,6 +7,9 @@ var partition = function(s) {
     const partitions = [];
     const isPalindrome = str => str === str.split('').reverse().join('');
    
+    // function isPalindrome(str){
+    //      str === str.split('').reverse().join('');
+    // }
     const findPalindrome = (str, start, parts, result) => {
         if (start === str.length) {
             result.push([...parts])
