5a98db84e8aed47597caffbb87060fd029fb2c5f
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-14 17:11:00 -0700

    Time: 81 ms (87.55%), Space: 44.2 MB (70.09%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -4,24 +4,36 @@
  * @return {number[][]}
  */
 var combinationSum2 = function(candidates, target) {
-    if(!candidates || candidates.length === 0) return [];   
-    let ans = [];
-    candidates = candidates.sort((a,b) => a-b);
+   /*
+   backtracking
+   1. edge cases(empty)
+   2. init ans arr
+   3. sort the input arr
+   3. init a helper func(sum, curArray, idx)
+   3-1) if sum = target, then put the curArrau into ans arr
+   3-2) if sum > target, then just return
+   3-3) iterate over the input arr and put each element into curArr
+   3-4) recursive the helper func(sum+curElement, curArr, idx+1)
+   3-5) pop the curArr
+   4. call helpfuc(0,[],0)
+   */
     
-    function backtracking(curSum, cur, idx){
-        if(curSum === target) return ans.push([...cur]);
+    if(!candidates) return [];
+    let ans = [];
+    candidates = candidates.sort((a,b) => a-b); 
+    
+    function backtracking(sum, cur, idx){
+        if(sum === target)  ans.push([...cur]);
+        if(sum > target ) return;
         
         for(let i=idx; i<candidates.length; i++){
-            if( i != idx && candidates[i] === candidates[i-1]) continue;
-            if(curSum > target) return;
-            
+            if(i > idx && candidates[i-1] === candidates[i]) continue;
             cur.push(candidates[i]);
-            backtracking(curSum + candidates[i], cur, i+1)
+            backtracking(sum+candidates[i], cur, i+1)
             cur.pop();
         }
-        
     }
     
-    backtracking(0, [] , 0);
+    backtracking(0, [], 0);
     return ans;
 }
