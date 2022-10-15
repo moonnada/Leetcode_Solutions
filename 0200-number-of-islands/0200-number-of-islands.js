b2139c6df410e50f7fecd3bf16523c002d53c2c3
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-15 10:12:21 -0700

    Time: 147 ms (41.40%), Space: 45.1 MB (57.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,27 +3,39 @@
  * @return {number}
  */
 var numIslands = function(grid) {
+ /*
+ backtracking dfs
+ 1. traverse 2d arr and find the first "land" and visit after the first land recursively. cnt is incremented
+ 2. in a helper func, check edge cases first. and then if cur position is land, then change to water to avoid revisit
+ 3. dfs 4 ways
+ */
+    
+    let row = grid.length;
+    let col = grid[0].length; 
     let cnt = 0;
-    for(let i=0; i<grid.length; i++){
-        for(let j=0; j<grid[0].length; j++){
+    
+    for(let i=0; i<row; i++){
+        for(let j=0; j<col; j++){
             if(grid[i][j] === "1"){
                 cnt++;
-                dfs(i,j, grid)
+                dfs(i,j,grid)
             }
         }
     }
-    return cnt
     
     function dfs(i,j, grid){
-        if(i<0 || i>=grid.length || j <0 || j>= grid[0].length) return;
+        if(i<0 || i>= grid.length || j<0 || j>=grid[0].length) return;
         
         if(grid[i][j] === "1"){
             grid[i][j] = "0";
-        } else return;
+            
+        } else return
         
-        dfs(i+1, j, grid);
-        dfs(i-1, j, grid);
-        dfs(i, j+1, grid);
-        dfs(i, j-1, grid)
+        dfs(i-1,j,grid);
+        dfs(i+1,j,grid);
+        dfs(i,j-1,grid);
+        dfs(i,j+1,grid);
     }
-};
+    
+    return cnt;
+}
