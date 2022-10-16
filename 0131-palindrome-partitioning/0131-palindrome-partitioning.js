b24707aee9c2d639cd9df307c9d70fda78a60fd2
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-15 23:02:15 -0700

    Time: 543 ms (19.72%), Space: 81.5 MB (33.37%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -3,35 +3,29 @@
  * @return {string[][]}
  */
 var partition = function(s) {
-    const output = [];
-    const partitions = [];
-    const isPalindrome = str => str === str.split('').reverse().join('');
-   
-    // function isPalindrome(str){
-    //      str === str.split('').reverse().join('');
-    // }
-    const findPalindrome = (str, start, parts, result) => {
-        if (start === str.length) {
-            result.push([...parts])
+    
+    let ans = [];
+    let partitions = [];
+    let isPalindrom = str => str === str.split('').reverse().join('');
+    
+    function findPalindrom(str, start, parts, result){
+        if(start === str.length){
+            result.push([...parts]);
             return;
         }
         
-        for (let i = start + 1; i <= str.length; i++) {
-            const target = str.substring(start, i);
-            if (isPalindrome(target)) {
+        for(let i=start+1; i<=str.length; i++){
+            let target = str.substring(start, i);
+            if(isPalindrom(target)){
                 parts.push(target);
-                findPalindrome(str, i, parts, result);        
+                findPalindrom(str, i, parts, result);
                 parts.pop();
             }
         }
     }
-    /*
-        string: 'aab'
-        start = 0 will find palindrome in 'a', 'aa', 'aab'
-        start = 1 will find palindrome in      'a',  'ab'
-        start = 2 will find palindrome in            'b'
-     */
-    findPalindrome(s, 0, partitions, output);   
     
-    return output;
+    findPalindrom(s, 0, partitions, ans);
+    return ans
+    
+    
 };
