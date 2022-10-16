881affef281a948af76e5618f13d6a7a1f72a57c
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-16 12:32:41 -0700

    Time: 102 ms (29.97%), Space: 44.2 MB (5.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -3,6 +3,13 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
+    /*
+    backtracking - dfs
+    DFS approach as we will check each permutation of characters and store them in ans array.
+    1. check edge cases(null, 0 length)
+    2. store characters into nums
+    3. in a dfs func, whenever 
+    */
     if(digits == null || digits.length === 0 ) return [];
     const map = {
         2: 'abc',
@@ -21,11 +28,13 @@ var letterCombinations = function(digits) {
             return ans.push(cur)
         }
         
-        let letters = map[digits[idx]]
+        let letters = map[digits[idx]];
         for(let char of letters){
             console.log(cur);
             dfs(idx+1, cur+char)
-        }
+        } 
+        
+       
     }
     
     dfs(0,[]);
