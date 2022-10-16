9b01c64c961ce79150937432bfa1676291b159b7
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-15 23:08:05 -0700

    Time: 495 ms (43.75%), Space: 80.6 MB (57.53%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -3,15 +3,19 @@
  * @return {string[][]}
  */
 var partition = function(s) {
-    
+    /*
+    1. init ans and partitions
+    2. check a str is palindrom
+    3. in a helpfunc, 
+    */
     let ans = [];
     let partitions = [];
     let isPalindrom = str => str === str.split('').reverse().join('');
     
     function findPalindrom(str, start, parts, result){
         if(start === str.length){
-            result.push([...parts]);
-            return;
+           return result.push([...parts]);
+           
         }
         
         for(let i=start+1; i<=str.length; i++){
