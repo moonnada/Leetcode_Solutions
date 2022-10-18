971ee6f39ae4933bc4e609d5aea16fed94ebcfe9
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-17 22:59:44 -0700

    Time: 122 ms (12.73%), Space: 43.8 MB (66.23%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.js
@@ -0,0 +1,36 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+    /*
+    1. check edge cases(empty, one node)
+    */
+    let start = node;
+    if(start === null) return null;
+    let map = new Map();
+    
+    let queue = [start];
+    map.set(start, new Node(start.val));
+    
+    while(queue.length > 0){
+        let curVal = queue.shift();
+        for(let neighbor of curVal.neighbors){
+            if(!map.has(neighbor)){
+                map.set(neighbor, new Node(neighbor.val));
+                queue.push(neighbor);
+            }
+            map.get(curVal).neighbors.push(map.get(neighbor));
+        }
+    }
+    
+    return map.get(start)
+};
