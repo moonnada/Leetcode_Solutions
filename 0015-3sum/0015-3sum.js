3b46786818c54473566cf72139f54f99ce5830d8
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-20 22:25:55 -0700

    Time: 226 ms (67.73%), Space: 52.2 MB (80.21%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,43 +3,47 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  /*
-  two pointers
-  1. sort the input arr
-  2. iterate the sorted arr. start from 0 to arr.length-2. the reason visting till length-2 is one of pointers will be assigned for the last num
-  3. check whether there is dulpicate num constantly. if is, continue;
-  4. init two ptrs(mid, end)
-  5. while mid < end, if sum of the three position values are eaqule to 0, then push to the ans arr. 
-    6. first of all, check there is duplicate num constantly on mid and end position
-    7. mid is incremented and end is decremented 
-  
-  */
+   /*
+   arr = [-1,0,1,2,-1,-4]
+   1. sort the input arr. [-4,-1,-1,0,1,2]
+   2. init a target val as 0
+   3. traverse the sorted arr from 0 to length-2. the reason length-2 is I am going to add the right pointer as nums.length -1 later
+   4. check if cur element > target, then break the loop. if cur element is a postive num, can't get the sum is 0
+   5. check if there is a duplicate element constantly, then continue. the reason is to escape getting a same format 
+   6. init the mid and right value
+   7. while(mid < right), init sum val = n[i] + n[j] + n[k]
+   8. if sum = target, then put the element into output arr
+   9.   also check there is same element constantly or not on mid place and right place both.
+   10. mid is incremented and right is decremented
+   10. else sum > target, right--, else mid++
+   => time : o(n2) since the for loop for the left place and while loop for the mid and end place
+      space : o(1)
+   */
     
-    nums = nums.sort((a,b) => a-b);
-    let ans =[];
+    let ans = [];
+    let target = 0;
+    nums.sort((a,b) => a-b);
     for(let i=0; i<nums.length-2; i++){
-        if(nums[i] > 0) break;
-        if(i>0 && nums[i] === nums[i-1]) continue;
+        if(nums[i] > target) break;
+        if(i> 0 && nums[i] === nums[i-1]) continue;
         
         let mid = i+1;
         let end = nums.length-1;
-        
         while(mid < end){
             let sum = nums[i] + nums[mid] + nums[end];
-            if( sum === 0){
+            if(sum === target){
                 ans.push([nums[i], nums[mid], nums[end]]);
-                 
+                
                 while(nums[mid] === nums[mid+1]) mid++;
                 while(nums[end] === nums[end-1]) end--;
-            
-                mid++; 
+                
+                mid++;
                 end--;
-            }else {
-                sum > 0 ? end-- : mid++;
             }
-            
-           
+            else {
+                sum > target ? end-- : mid++;
+            }
         }
     }
-    return ans;
+    return ans
 }
