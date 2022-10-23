e73289654cb3927b70109dd17eab40ae08a3c667
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-23 14:01:15 -0700

    Time: 157 ms (40.83%), Space: 46.1 MB (91.83%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.js
+++ 0130-surrounded-regions/0130-surrounded-regions.js
@@ -3,32 +3,47 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
-    //check border lines first.
-    for(let i = 0; i < board.length; i++) {
-        for(let j = 0; j < board[0].length; j++) {
-            if(board[i][j] === 'O' && (i === 0 || j === 0 || i === board.length - 1 || j === board[0].length - 1)) dfs(i, j);
+   /*
+   DFS
+   1. traverse 2d arrs to find 'x's in boards
+    2. if find 'x' in boards, then deep inside by using a helper func
+    
+    3. in a helper func, check edge cases first.
+        4. curPostion is marked to 'visited'
+        5. visit 4 different ways
+    
+    6. traverse 2d arr to change chars.
+        7. if position is marked as 'visited', then change to 'x', if position is marked as 'o', changed to 'x'
+   */
+    
+    let row = board.length;
+    let col = board[0].length;
+    
+    for(let i=0; i<row; i++){
+        for(let j=0; j<col; j++){
+            if(board[i][j] === 'O' && (i=== 0 || j === 0 || i=== row-1 || j === col-1)) dfs(i,j);
         }
     }
-
-    for(let i = 0; i < board.length; i++) {
-        for(let j = 0; j < board[0].length; j++) {
-            if(board[i][j] === 'Visited') {
+    
+    function dfs(i,j){
+        if(i < 0 || i>= row || j < 0 || j>= col || board[i][j] !== 'O') return;
+        
+        board[i][j] = 'visited';
+        dfs(i-1,j);
+        dfs(i+1, j);
+        dfs(i,j-1);
+        dfs(i,j+1);
+    }
+    
+    for(let i=0; i<row; i++){
+        for(let j=0; j<col; j++){
+            if(board[i][j] === 'visited'){
                 board[i][j] = 'O';
-            } else  {
-                board[i][j] = 'X';
+            } else if( board[i][j] === 'O'){
+                board[i][j] = 'X'
             }
-        }    
+        }
     }
-    //mark 'visited when 'O' is on borders. if the 'O' is connected to middle point, then the place is useless point.
-    function dfs(i, j) {
-        if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] === 'Visited' || board[i][j] === 'X') return
-        
-        board[i][j] = 'Visited';
-        dfs(i + 1, j);
-        dfs(i - 1, j);
-        dfs(i, j + 1);
-        dfs(i, j - 1);
-        
-        return;
-    }
+    
+    return board;
 };
