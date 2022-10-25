0b1abd67cacbcb1e7b91cd4815c5f6973bec65ae
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-24 22:29:40 -0700

    Time: 124 ms (65.29%), Space: 46.4 MB (83.69%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -7,6 +7,7 @@ var canFinish = function(numCourses, prerequisites) {
     let preMap = {};
     let visited = {};
     
+    
     for(let i=0; i<prerequisites.length; i++){
         if(preMap[prerequisites[i][0]] === undefined){
             preMap[prerequisites[i][0]] = [prerequisites[i][1]];
@@ -16,13 +17,20 @@ var canFinish = function(numCourses, prerequisites) {
     }
     
     function dfs(node){
+        //if already visit the cur node, return false
         if(visited[node]) return false;
         
+        //if cur node has not visited yet
         if(preMap[node] !== undefined){
+            
+            //if curNode of map is empty (means reaching the end of node), return true
             if(preMap[node].length === 0) return true;
             
+            //mark cur node as visited
             visited[node] = true;
             
+            //traverse curNode of map to see there is no re-occurance of the curNode.
+            //if dfs func returns false, then return false
             for(let val of preMap[node]){
                 if(!dfs(val)) return false;
             }
@@ -34,6 +42,7 @@ var canFinish = function(numCourses, prerequisites) {
     }
     
     for(let key in preMap){
+        //if dfs func returns false, then return false
         if(!dfs(key)) return false;
     }
     return true;
