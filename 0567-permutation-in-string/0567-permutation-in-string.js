d61baab4f0a242b0811a3d9b6ff00b26d431b748
moonnada <53587841+moonnada@users.noreply.github.com>
2022-10-25 21:18:22 -0700

    Time: 96 ms (91.59%), Space: 45.1 MB (61.53%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -5,42 +5,37 @@
  */
 var checkInclusion = function(s1, s2) {
     /*
-    sliding window. time: o(n), space: o(n)
-    1. edge cases
-    2. init an object to store the frequency of s1's char
-    3. init a left and right pointer, requiredLength
-    4. iterate a loop to put s1's char and frequency into the object
-    5. while right < s2.length, if find a s1's char in s2's index, then decrement the requiredLength
-    6. since we have encountered new char, s2[right] is decremented and increment to the right side
-    7. if requiredLength = 0, return true
-    8. if right - left = s1.length => this means length is same, but have different val. then we have to remove left element of window.
-    9. end of loop, return false
-    */
-    if (s1.length > s2.length) return false;
+ sliding window
+ 1. edge cases
+ 2. init an object to store s1's char
+ 3. iterative a loop to put s1's char into object
+ 4. init left and right ptrs
+ 5. while right < s2.length
+    6. if s2's char is in object, then decrease requiredlength
+    7. 
+    7. if requiredlength = 0, return true;
+    8. 
+  */
+    if(s1.length > s2.length) return false;
     let needed = {};
     for(let i=0; i<s1.length; i++){
-        // visited[char] = visited[char] ? visited[char]+1 : 1;
-        needed[s1[i]] = needed[s1[i]] ? needed[s1[i]] + 1 : 1
-    }
+        needed[s1[i]] = needed[s1[i]] ? needed[s1[i]]+ 1 : 1
+    };
     
-    let left =0, right=0, requiredLength = s1.length;
+    let left=0, right=0, requiredLength = s1.length;
     
     while(right < s2.length){
-        if(needed[s2[right]] > 0){
-           requiredLength--;
-        }
+        if(needed[s2[right]] > 0) requiredLength--;
         
         needed[s2[right]]--;
         right++;
+        
         if(requiredLength === 0) return true;
         
-        if(right - left === s1.length){
-           if(needed[s2[left]] >= 0) {
-               requiredLength++;
-           }
+        if(right-left === s1.length){
+            if(needed[s2[left]] >=0) requiredLength++;
             needed[s2[left]]++;
             left++;
         }
-    }
-    return false
+    } return false
 }
