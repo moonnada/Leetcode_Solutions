ddabe3a039720f9aaa946747ef4e82df69399aff
moonnada <53587841+moonnada@users.noreply.github.com>
2023-02-26 23:20:27 -0800

    Time: 78 ms (87.20%), Space: 43.4 MB (73.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -9,8 +9,10 @@ var isAnagram = function(s, t) {
     for(let i=0; i<s.length; i++){
         map[s[i]] = map[s[i]] ? map[s[i]] + 1 : 1;
     }
+    
     for(let i=0; i<t.length; i++){
         if(!map[t[i]]) return false;
         map[t[i]]--;
-    } return true
-};
+    }
+    return true
+}
