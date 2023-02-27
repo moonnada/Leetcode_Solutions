c42099cc28d9191ad3d70449d6bdc4576d38aa0c
moonnada <53587841+moonnada@users.noreply.github.com>
2023-02-26 23:49:30 -0800

    Time: 56 ms (98.79%), Space: 43.2 MB (25.40%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    /*
+    hashmap
+    1. init a map
+    2. during traversing the input arr using a loop, 
+        init a looking val as target - nums element
+        if map has a looking val, return as arr like [ map.get(looking val) , cur indice]
+        else map.set ( num[indice] , 1)
+    */
+
     let map = new Map();
     for(let i=0; i<nums.length; i++){
-        if(map.has(target - nums[i])){
-            return [ map.get(target - nums[i]) , i]
-        } else {
-            map.set(nums[i], i)
-        }
+        let lookFor = target - nums[i];
+        if(map.has(lookFor)) return [ map.get(lookFor), i];
+        else map.set(nums[i], i);
     }
+    
 };
