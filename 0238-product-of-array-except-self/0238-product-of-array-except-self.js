2c3d51137b01f5aff8377580e4c1cd45b68d4e5e
moonnada <53587841+moonnada@users.noreply.github.com>
2023-02-28 11:09:25 -0800

    Time: 107 ms (74.01%), Space: 55.5 MB (35.42%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,20 +3,35 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let left = [];
-    let right = [];
-    let startLeft = 1;
-    let startRight =1;
+//     let left = [];
+//     let right = [];
+//     let startLeft = 1;
+//     let startRight =1;
     
+//     for(let i=0; i<nums.length; i++){
+//         left[i] = startLeft;
+//         startLeft *= nums[i]
+//     }
+    
+//     for(let i=nums.length-1; i>=0; i--){
+//         right[i] = startRight;
+//         startRight *= nums[i];
+//         right[i] *= left[i];
+//     }
+//     return right
+    
+    let ans = [];
+    let startLeft = 1;
+    let startRight = 1;
     for(let i=0; i<nums.length; i++){
-        left[i] = startLeft;
-        startLeft *= nums[i]
+        ans[i] = startLeft
+        startLeft *= nums[i];
     }
     
     for(let i=nums.length-1; i>=0; i--){
-        right[i] = startRight;
+        ans[i] *= startRight;
         startRight *= nums[i];
-        right[i] *= left[i];
     }
-    return right
+    
+    return ans;
 };
