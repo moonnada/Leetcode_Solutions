91bc6e129166d426b92259fe1cccbfde7bd909c9
moonnada <53587841+moonnada@users.noreply.github.com>
2023-03-07 22:39:21 -0800

    Time: 71 ms (48.78%), Space: 42.8 MB (82.00%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,23 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    /*
-    Two pointers
-    return their element position+1
-    1. init two pointers. left and right
-    2. iterative the input arr to find ans
-    2. if cur two pointers sum = target, return the two pointer+1 each place
-    3. else if sum>target, right--, else left++
-    */
-    
     let left = 0;
-    let right = numbers.length-1;
+    let right = numbers.length -1;
     
-    while(left < right){
-        let sum = numbers[left] + numbers[right];
-        if(sum === target){
-            return [left+1, right+1];
-        } 
-        sum > target ? right-- : left++;
+    while( left < right){
+        let sum = numbers[left] + numbers[right]
+        if( sum === target) return [left+1, right+1]
+       sum > target ? right-- : left++
     }
 };
