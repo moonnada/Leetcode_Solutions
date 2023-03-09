28e51771b5797d4a1117244176814594a0e7c098
moonnada <53587841+moonnada@users.noreply.github.com>
2023-03-08 22:59:52 -0800

    Time: 191 ms (52.85%), Space: 56.1 MB (72.65%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,47 +3,35 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-   /*
-   arr = [-1,0,1,2,-1,-4]
-   1. sort the input arr. [-4,-1,-1,0,1,2]
-   2. init a target val as 0
-   3. traverse the sorted arr from 0 to length-2. the reason length-2 is I am going to add the right pointer as nums.length -1 later
-   4. check if cur element > target, then break the loop. if cur element is a postive num, can't get the sum is 0
-   5. check if there is a duplicate element constantly, then continue. the reason is to escape getting a same format 
-   6. init the mid and right value
-   7. while(mid < right), init sum val = n[i] + n[j] + n[k]
-   8. if sum = target, then put the element into output arr
-   9.   also check there is same element constantly or not on mid place and right place both.
-   10. mid is incremented and right is decremented
-   10. else sum > target, right--, else mid++
-   => time : o(n2) since the for loop for the left place and while loop for the mid and end place
-      space : o(1)
-   */
-    
-    let ans = [];
-    let target = 0;
-    nums.sort((a,b) => a-b);
-    for(let i=0; i<nums.length-2; i++){
-        if(nums[i] > target) break;
-        if(i> 0 && nums[i] === nums[i-1]) continue;
-        
-        let mid = i+1;
-        let end = nums.length-1;
-        while(mid < end){
-            let sum = nums[i] + nums[mid] + nums[end];
-            if(sum === target){
-                ans.push([nums[i], nums[mid], nums[end]]);
-                
-                while(nums[mid] === nums[mid+1]) mid++;
-                while(nums[end] === nums[end-1]) end--;
-                
-                mid++;
-                end--;
-            }
-            else {
-                sum > target ? end-- : mid++;
+     // sort for simpler detection of duplicates
+    const numbers = nums.sort((a, b) => a - b);
+
+    const triplets = [];
+    for (let i = 0; i < numbers.length; i++) {
+        // prevent duplicates
+        if (i > 0 && numbers[i - 1] === numbers[i]) {
+            continue;
+        }
+
+        // find sum with 2-pointer method
+        let left = i + 1;
+        let right = numbers.length - 1;
+        while (left < right) {
+            const sum = numbers[i] + numbers[left] + numbers[right];
+            if (sum === 0) {
+                triplets.push([numbers[i], numbers[left], numbers[right]]);
+                // shift pointer to a new value to prevent duplicates
+                left++;
+                while (numbers[left] === numbers[left - 1] && left < right) {
+                    left++;
+                }
+            } else if (sum > 0) {
+                right--;
+            } else {
+                left++;
             }
         }
     }
-    return ans
-}
+
+    return triplets;
+};
