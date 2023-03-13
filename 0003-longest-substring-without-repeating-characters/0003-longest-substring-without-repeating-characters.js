f960806343ac897e4ba4286264d5188d63d5b33f
moonnada <53587841+moonnada@users.noreply.github.com>
2023-03-13 00:14:48 -0700

    Time: 80 ms (89.86%), Space: 46.4 MB (80.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,29 +3,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    /*
-    sliding windows. Time: O(n), Space: O(n)
-    1. init a map(key: curElement, val: curIndex)
-    2. init basic vals(maxLen, start)
-    3. traverse the input string from 0 to the end
-        3-1) if map has curElement, then update start
-        3-2) update map with curElement and index
-        3-3) get maxLen
-    4. return maxLen
-    */
+     let set = new Set();
+    let left = 0;
+    let max = 0;
     
-    let map = new Map();
-    let start=0, maxLen=0;
-    for(let end=0; end<s.length; end++){
-        if(map.has(s[end])){
-            //if the cur char was seen, move the start to (1 + the last index of this char )
-            //max prevents moving backward, 'start' can only move forward
-            start = Math.max( map.get(s[end])+1, start)
+    for(let i=0; i<s.length; i++){
+        while(set.has(s.charAt(i))){
+            set.delete(s.charAt(left));
+            left+=1;
         }
-        map.set(s[end], end);
-        maxLen = Math.max(maxLen, end-start+1)
+        set.add(s.charAt(i));
+        max = Math.max(max, i-left + 1);
     }
-    return maxLen
-   
-   
-}
+    return max
+};
