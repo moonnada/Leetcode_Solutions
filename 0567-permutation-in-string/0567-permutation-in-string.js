5f0f39007bd577d94b5839e4176b41f457eb89cc
moonnada <53587841+moonnada@users.noreply.github.com>
2023-03-14 00:56:44 -0700

    Time: 82 ms (79.06%), Space: 45.1 MB (58.78%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -4,18 +4,6 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    /*
- sliding window
- 1. edge cases
- 2. init an object to store s1's char
- 3. iterative a loop to put s1's char into object
- 4. init left and right ptrs
- 5. while right < s2.length
-    6. if s2's char is in object, then decrease requiredlength
-    7. 
-    7. if requiredlength = 0, return true;
-    8. 
-  */
     if(s1.length > s2.length) return false;
     let needed = {};
     for(let i=0; i<s1.length; i++){
@@ -25,17 +13,22 @@ var checkInclusion = function(s1, s2) {
     let left=0, right=0, requiredLength = s1.length;
     
     while(right < s2.length){
-        if(needed[s2[right]] > 0) requiredLength--;
+        if(needed[s2[right]] > 0) {
+            requiredLength--;
+        }
+        if(requiredLength === 0) return true;
         
         needed[s2[right]]--;
         right++;
         
-        if(requiredLength === 0) return true;
+        
         
         if(right-left === s1.length){
-            if(needed[s2[left]] >=0) requiredLength++;
+            if(needed[s2[left]] >=0) { 
+                requiredLength++;
+            }
             needed[s2[left]]++;
             left++;
         }
     } return false
-}
+};
