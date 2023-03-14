307fe0c100a8bb9ee16bc78cbfc683d8ce9acfe0
moonnada <53587841+moonnada@users.noreply.github.com>
2023-03-14 00:10:49 -0700

    Time: 66 ms (98.19%), Space: 42.7 MB (53.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,18 +4,20 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-   let left = 0 , right = 0, max = 0;
-    const map = new Map();
+   let left = 0, right = 0, max = 0;
+    let map = new Map();
+    
     while(right < s.length){
-        let char = s[right];
-        map[char] =  map[char]+1 || 1;
+        let curChar = s[right];
+        map[curChar] = map[curChar] + 1 || 1;
         
-        max = Math.max(max, map[char]);
+        max = Math.max(max, map[curChar]);
         
-        if(right-left+1 - max > k){
-            map[[s[left]]]--;
+        if(right - left + 1 - max > k) {
+            map[s[left]]--;
             left++;
         } right++;
+        
     }
     return right-left
 };
