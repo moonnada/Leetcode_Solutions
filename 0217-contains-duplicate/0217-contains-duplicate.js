0d15cd17c395c03210e0f681baaf3fee8fa2a4d2
moonnada <53587841+moonnada@users.noreply.github.com>
2023-05-24 16:41:39 -0700

    Time: 82 ms (91.17%), Space: 59.6 MB (5.19%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -17,7 +17,7 @@ var containsDuplicate = function(nums) {
     */
     let map = new Map();
     for(let i=0; i<nums.length; i++){
-        if(map.has(nums[i])) return true;
-        else map.set(nums[i])
+         if (map.has(nums[i]) )return true;
+        map.set(nums[i])
     } return false
 }
