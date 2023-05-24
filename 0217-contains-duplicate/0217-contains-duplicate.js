5255213a62cc218bc556302e72e5a8d574abeb4f
moonnada <53587841+moonnada@users.noreply.github.com>
2023-05-24 16:32:20 -0700

    Time: 95 ms (57.12%), Space: 59.7 MB (5.19%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,10 +3,21 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
+   /*
+   U:
+   q) if input arrary is empty, what do i need to return
+   
+   M: map (key: num, val: cnt)
+   P: 
+   1. init a map
+   2. traverse the input arr and if map has a same val, then return true. if not put key and val into the map
+   3. out of loop, return false
+   
+   
+    */
     let map = new Map();
     for(let i=0; i<nums.length; i++){
         if(map.has(nums[i])) return true;
-        map.set(nums[i])
-    }
-    return false
+        else map.set(nums[i])
+    } return false
 }
