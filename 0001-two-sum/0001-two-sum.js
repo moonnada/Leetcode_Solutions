6a9d838f820657a831bede644dbde122956d766c
moonnada <53587841+moonnada@users.noreply.github.com>
2023-05-24 23:06:39 -0700

    Time: 66 ms (75.24%), Space: 44.4 MB (13.10%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -17,7 +17,10 @@ var twoSum = function(nums, target) {
     */
     let map = new Map();
     for(let i=0; i<nums.length; i++){
-        if(map.has( target - nums[i])) return [ map.get(target-nums[i]) , i];
-        map.set(nums[i] , i);
+        if(map.has( target - nums[i])) {
+            console.log(map.get(target-nums[i]))
+            return [  map.get(target-nums[i]), i];
+        }
+        map.set(nums[i],i );
     }
 };
