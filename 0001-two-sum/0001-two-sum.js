8c2015f3de41540888159816606ef5152d108fa4
moonnada <53587841+moonnada@users.noreply.github.com>
2023-05-24 23:02:27 -0700

    Time: 70 ms (66.53%), Space: 44.6 MB (11.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,26 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  /*
-  q1. input arr has any integer nums? even negative num?
-  q2. what if target is 0, and input arr has 0 elements
-  
-  m : hashmap (key: array's element, val: each element's count)
-  
-  1. init a hashamp
-  2. traverse the input arr and initialize lookingval. 
-  3.    if map has the lookingVal, then just return the curIndice and lookingVal's element
-  4.    else map.set(lookingVal and indice)
-  */
-
+    /*
+        U:
+        q) negative num could be a nums val?
+        
+        M: map => key as indice, val as element
+        P: 
+            1. init a map
+            2. traverse the input arr and 
+                if map has target - curVal, return curIndice and map's indice
+                else put indice and num into map
+    */
     let map = new Map();
     for(let i=0; i<nums.length; i++){
-        let lookFor = target - nums[i];
-        if(map.has(lookFor)){
-            return [ map.get(lookFor), i];
-        }
-        map.set(nums[i], i);
+        if(map.has( target - nums[i])) return [ map.get(target-nums[i]) , i];
+        map.set(nums[i] , i);
     }
-  
-    
 };
