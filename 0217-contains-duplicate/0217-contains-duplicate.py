85cae7d314f64ee496ef0331c16aab89a4df0b11
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-07 18:13:48 -0700

    Time: 472 ms (83.70%), Space: 27.5 MB (76.11%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dic = {}
+        for num in nums:
+            if num in dic: 
+                return True
+            else:
+                dic[num] = 1
+                
+        return False
+            
+        
