f6f4caa396b546ff8784a2287ac8381d855c27a1
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-08 11:52:30 -0700

    Time: 67 ms (48.87%), Space: 16.9 MB (47.74%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        '''
+        U: 
+        q) can input str be empty?
+        q) any letter can be in input str?
+        
+        M: 
+        hashmap (key: num, val: cnt)
+        
+        p:
+        1. check each str's length and if they are different, return false
+        2. init maps for the two input strs to count
+        3. traverse the first input str to put key and value inside a map. put input s, t both
+        4. traverse the input s'map to compare t's map count
+        
+        '''
+        
+        if len(s) != len(t): return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+                
+        for cnt in countS:
+            if countS[cnt] != countT.get(cnt, 0): return False
+            
+        return True
+    
