952b9cb178beefb1894feb9844b01817c8647ce2
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-09 12:42:17 -0700

    Time: 132 ms (22.37%), Space: 22.4 MB (7.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
-        for s in strs:
-            count = [0]*26
-            for char in s:
-                count[ord(char) - ord('a')] += 1
-            ans[tuple(count)].append(s)
+        '''
+            1. init a map and mapping charCount to list of anagrams
+            2. iterate a loop to get count by using unicode
+            3. inside a loop, create a new loop to count each letter
+            4. append cur word into map
+            5. return map's val
+        '''
+        
+        hashmap = collections.defaultdict(list)
+        for str in strs:
+            count = [0] * 26
+            for cnt in str:
+                count[ord(cnt) - ord('a')] += 1
             
-        return ans.values()
+            hashmap[tuple(count)].append(str)
+            
+        return hashmap.values()
+            
