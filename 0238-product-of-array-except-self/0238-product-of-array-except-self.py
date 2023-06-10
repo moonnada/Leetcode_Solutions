deb2a09bb3fe2e15fd4f57eda084c5781c23dec9
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-10 14:52:30 -0700

    Time: 237 ms (66.33%), Space: 23.5 MB (74.16%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,42 +4,26 @@ class Solution:
         U:  q) input arr can be empty?
             q) input arr is sorted?
             
-            [1,2,3,4]
-             1 1 2 6
-             
-            24  12  4  1
-            
-        =>  24 12 8 6
-        
-        
-             [1,2,3,4]
-             1 1 2 6
-                 8 6
                  
-        M: two arr(start from left and from right)
+        M: make an index for pre and post
         
         P:
-            1. init an arr for left to right
-            2. iterate a loop to get product 
-            3. init an arr for right to left
-            4. iterate a loop to get product
-            5. get each product to combine left and right arrs
-                
+            1. init ans arr 
+            2. init a preIndex as 1 and get product by iterating a loop
+            3. init a postIndex as 1 and get ans by iterating a loop
+            
         '''
         
-        length = len(nums)
+        ans = [1] * len(nums)
+        preIndex = 1
+        for i in range(len(nums)):
+            ans[i] = preIndex
+            preIndex *= nums[i]
         
-        left, right, ans = [0]*length, [0]*length,[0]*length
-        
-        left[0] = 1
-        for i in range(1, length):
-            left[i] = nums[i-1] * left[i-1]
-            
-        right[length-1] = 1
-        for i in reversed(range(length-1)):
-            right[i] = nums[i+1] * right[i+1]
-            
-        for i in range(length):
-            ans[i] = left[i] * right[i];
+        postIndex = 1
+        for i in reversed(range(len(nums))):
+            ans[i] *= postIndex
+            postIndex *= nums[i]
             
         return ans
+        
