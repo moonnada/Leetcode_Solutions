4fc2c57d51228e41bdfc675d328b7a69f0690d9a
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-12 14:40:53 -0700

    Time: 179 ms (51.15%), Space: 17.9 MB (40.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -20,7 +20,7 @@ class Solution:
         '''
         left = 0
         for right in range(len(nums)):
-            if nums[right]:
+            if nums[right] !=0:
                 nums[left], nums[right] = nums[right], nums[left]
                 left+= 1
         return nums
