756bec6dba2d93c9f1c1dbdfe0debe2993f0f144
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-14 15:50:03 -0700

    Time: 532 ms (93.77%), Space: 17.1 MB (35.23%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -11,18 +11,21 @@ class Solution:
         M: sliding window
         
         P: 
-            1. traverse the input arr
-            2. if curVal is 1, cnt++
-            3. if curVal is not 0 and k > 0, cnt++ and k--
-            4. while k == 0, left++, k--
-            
-            
+            1. init a left as 0
+            2. traverse the input arr
+                2.1) k -= 1- curVal (if rightpt is 0, then k is decremented. if rightpt is 1, then k is remained)
+                2.2) if k < 0, k += 1 - leftpt (if curVal is 0, then k is incremented. if leftpt is 1, then k is remained)
+                2.3) left is incremented
+            3. return right - left + 1
         '''
+        left = 0
         
-        i = 0
-        for j in range(len(nums)):
-            k -= 1 - nums[j]
+        for right in range(len(nums)):
+            k -= 1- nums[right]
+        
             if k < 0:
-                k += 1 - nums[i]
-                i += 1
-        return j - i + 1
+                k += 1- nums[left]
+                left+=1
+                
+        return right-left+1
+        
