8bd39b1e1d1653be4abc17ebe47ca2cf36807a41
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-14 14:07:33 -0700

    Time: 240 ms (25.60%), Space: 17.4 MB (33.73%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -11,23 +11,31 @@ class Solution:
         M: sliding window
         
         P:
-            1. init a set and put letters of input str within 'k' val into the set
-            2. make a str for vowel letters
-            3. 
+            1. init a str for vowel letters
+            1. init values( cnt, cur, left) as 0
+            3. traverse the input arr
+                3.1) if a vowel is found in a position, then cnt ++
+                3.2) if traversed length > k 
+                3.3)       if cur position has a vowel, then cnt--
+                3.4) left++
+                3.5) get max
+            4. return max
+                
         '''
-        VOWELS = frozenset('aeiou')
+      
+        VOWELS = 'aeiou'
         
-        cnt=cur= left = 0
+        maxVowels = cur = left = 0
         
         for right in range(len(s)):
             if s[right] in VOWELS:
-                cur += 1
+                cur+= 1
                 
             if right - left + 1 > k:
                 if s[left] in VOWELS:
-                    cur -= 1
-                left += 1
-                
-            cnt = max(cnt, cur)
+                    cur-=1
+                left +=1
             
-        return cnt
+            maxVowels = max(cur , maxVowels)
+            
+        return maxVowels
