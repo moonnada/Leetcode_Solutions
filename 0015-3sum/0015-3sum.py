32c522ea75da7be54693bde2df3389578d58a0b1
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-15 13:11:16 -0700

    Time: 1405 ms (70.52%), Space: 20.6 MB (40.37%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -12,7 +12,6 @@ class Solution:
                 [-4,-1,-1,0,1,2]
                   L           R
                   
-                  
         M: two ptrs
         
         P: 
@@ -26,27 +25,76 @@ class Solution:
                 2.6) else put three ptrs in to ans arr
                 2.7) left ptr is incremented to avoid adding duplicate
                 2.8) make another loop to avoid duplicate 
+                
+                
+          
+          1. sort input arr
+          2. traverse the arr to find 3sum is equal to 0
+            2.1) if curIndex > 0 and curVal is same num as the previous one, then just move to the next step
+            2.2) init two ptrs
+            2.3) while left < right:
+            2.4) if curthreeSum > 0: right--
+            2.5) elseif < 0: left++
+            2.6) else add the three ptrs into ans
+            2.7) to avoid adding duplicate nums, check if there is a same num
         '''
         
         ans = []
         nums.sort()
         
-        for i,val in enumerate(nums):
-            if i>0 and val == nums[i-1]: continue
+        for i, val in enumerate(nums):
+            if i > 0 and val == nums[i-1]: continue
             
             left, right = i+1, len(nums)-1
             
             while left < right:
                 threeSum = val + nums[left] + nums[right]
-                if threeSum > 0:
-                    right -= 1
-                elif threeSum < 0:
+                if threeSum < 0:
                     left += 1
+                elif threeSum > 0:
+                    right -= 1
                 else:
-                    ans.append([val, nums[left], nums[right]])
+                    ans.append([val, nums[left],nums[right]])
                     left += 1
                     
                     while nums[left] == nums[left-1] and left < right:
-                        left+=1
-                        
+                        left+= 1
+                    
         return ans
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ans = []
+#         nums.sort()
+        
+#         for i,val in enumerate(nums):
+#             if i>0 and val == nums[i-1]: continue
+            
+#             left, right = i+1, len(nums)-1
+            
+#             while left < right:
+#                 threeSum = val + nums[left] + nums[right]
+#                 if threeSum > 0:
+#                     right -= 1
+#                 elif threeSum < 0:
+#                     left += 1
+#                 else:
+#                     ans.append([val, nums[left], nums[right]])
+#                     left += 1
+                    
+#                     while nums[left] == nums[left-1] and left < right:
+#                         left+=1
+                        
+#         return ans
