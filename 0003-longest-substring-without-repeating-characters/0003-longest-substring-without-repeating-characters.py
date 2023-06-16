5a4c778343a11e338900048799517119d2b6455f
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-15 18:07:25 -0700

    Time: 75 ms (64.63%), Space: 16.5 MB (35.21%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,42 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        '''
+        U:
+        q) can input str be empty?
+        q) can it distinguish upper and lower cases?
+        
+        
+        ex) "pwwkew"
+             LR
+               R
+               
+        ex) "abcabcbb"     
+             LR
+             
+        M: sliding window, hashset
+        
+        P: 
+            1. check edge cases(empty, one letter)
+            2. init a set and leftptr
+            3. traverse the input str
+                3.1) while curVal is in set, then remove the val in the set and left++
+                3.2) out of while loop(which means curVal is not in the set), put the curVal in the set
+                3.3) get max length
+                            
+        '''
+        
+        if len(s) == 0 or len(s)==1: return len(s)
+        
+        left = maxCnt = 0
+        charSet = set()
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+                
+            charSet.add(s[right])
+            maxCnt = max(maxCnt, right-left+1)
+        
+        return maxCnt
+        
+        
