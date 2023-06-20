904a665c4d56c12c4c8a31a39b093f0ce945e321
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-19 20:03:49 -0700

    Time: 285 ms (44.50%), Space: 18.1 MB (35.01%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -5,9 +5,11 @@ class Solution:
             q) what if str has only *?
         
         P:
-            1. check edge case(empty)
+            1. check edge cases(empty..)
             2. init an arr for answer
             3. traverse the input str to put elements into arr
+                3.1) check whether curVal is *. if it isnt, add the curVal into arr. else remove the arr's element
+            4. return ans
         '''
         
         
@@ -17,8 +19,10 @@ class Solution:
         ans=[]
         
         for i in s:
-            if i!='*':
-                ans+=[i]
-            else:
+            if i =='*':
                 ans.pop()
+            else:
+                ans+=[i]
+
+                
         return "".join(ans)
