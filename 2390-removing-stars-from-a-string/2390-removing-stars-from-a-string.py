40bb6d4547fb03ffd4cb92e0cd1c2ea9ad456325
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-19 19:47:57 -0700

    Time: 286 ms (44.14%), Space: 17.9 MB (64.66%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -1,12 +1,24 @@
 class Solution:
     def removeStars(self, s: str) -> str:
-        ans=[]
+        '''
+        U: 
+            q) what if str has only *?
+        
+        P:
+            1. check edge case(empty)
+            2. init an arr for answer
+            3. traverse the input str to put elements into arr
+        '''
+        
+        
         
         if len(s) == 0: return ""
         
+        ans=[]
+        
         for i in s:
-            if i=='*':
-                ans.pop()
-            else:
+            if i!='*':
                 ans+=[i]
+            else:
+                ans.pop()
         return "".join(ans)
