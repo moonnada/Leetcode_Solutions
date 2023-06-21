c9c4ccc6f0df0105de13f018ec827bd7f70ef9cd
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-21 13:34:09 -0700

    Time: 161 ms (65.44%), Space: 17.6 MB (62.75%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -2,27 +2,31 @@ class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         '''
         U:
-        nums = [1, 7, 3, 6, 5, 6]
-      leftSum : 1  8  11 17 22 28
-      rightSum :28   27    20  17  11  6
-               
-           
+        ex) nums = [1, 7, 3, 6, 5, 6]
+            totalsum = 28
+            leftsum = 0
+        M: prefix
            
         P:
-        
-            - init two ptrs(left and right)
-            - while left < right
-                - leftSum += curLeft, left++
-                - rightSum += curRight, right--
-                
-                if leftSum == rightSum
+            0. check edge cases(empty, one element)
+            1. get totalSum
+            2. init a leftSum as 0
+            3. traverse the input list to find a pivot
+                3.1) get a rightSum. rightSum = totalSum - leftSum - curIndex
+                3.2) if leftSum == rightSum, return curIndex
+                3.3) else curElement is added to the leftSum
+            4. return -1
+           
         '''
+        
         totalSum = sum(nums)
         leftSum = 0
-        
         for i in range(len(nums)):
-            rightSum = totalSum - nums[i] - leftSum
+            rightSum = totalSum - leftSum - nums[i]
             if leftSum == rightSum: return i
             else: leftSum += nums[i]
+                
         return -1
         
+        
+     
