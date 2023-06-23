1055ced75cd5c0d4ebe5e6459aabec70a3c63b64
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-23 14:16:31 -0700

    Time: 78 ms (85.27%), Space: 16.4 MB (86.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -13,29 +13,31 @@ class Solution:
         
         P:
             1. check edge cases(empty, s1 and s2 length)
-            2. permutate s1
-            3. traverse the input s2 to find s1's permutation
+            2. init a global values of count freq of both strs
+            3. traverse the input str1 to get freq
+            4. if both freq are equal, then reture true\
+            5. traverse the input str2 to slide a window
+                5.1) sliding window is incremented
+                5.2) remove the left side of window
+                5.3) if freq of both strs are equal, then return
+            6. return false
         '''
-        if len(s1) > len(s2):
-            return False
+        if len(s1) > len(s2): return False
         
-        s1freq =  [0]*26
-        s2freq =  [0]*26
+        freqS1 = [0] * 26
+        freqS2 = [0] * 26
         
         for i in range(len(s1)):
-            s1freq[ord(s1[i])-97] += 1
-            s2freq[ord(s2[i])-97] += 1 
+            freqS1[ord(s1[i]) - 97] += 1
+            freqS2[ord(s2[i])- 97] += 1
             
-        if s1freq == s2freq:
+        if freqS1 == freqS2:
             return True
         
-        for i in range(len(s1),len(s2)):
+        for i in range(len(s1), len(s2)):
+            freqS2[ord(s2[i]) - 97] += 1
+            freqS2[ord(s2[i - len(s1)]) - 97] -= 1
             
-            s2freq[ord(s2[i])-97] += 1
-            s2freq[ord(s2[i-len(s1)])-97] -= 1
+            if freqS1 == freqS2: return True
             
-            if s1freq == s2freq:
-                return True
-        
         return False
-        
