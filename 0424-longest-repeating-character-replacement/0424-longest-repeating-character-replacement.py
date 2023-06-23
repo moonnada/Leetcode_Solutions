8513eea99f1a30765690e32e3849667bf6a252e1
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-22 19:06:05 -0700

    Time: 121 ms (81.65%), Space: 16.5 MB (7.21%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,32 +5,28 @@ class Solution:
         q) can input str or k be empty || 0?
         Q) input str have only upper letter?
         
-        ex) "AABABBB", k = 1
-             L
-                R
-             
-             for r in s
-                 if s[l] == s[r], cnt++
-                 else
-                    while k >0,
-                        k--. cnt++
-
-                maxCnt = max(cnt, maxCnt)
-             
-             
+        M: hashmap, sliding window
         
+        P:
+            1. init global values( first ptr, map, countFreq, strLen)
+            2. iterate over the input str
+                2.1) count each char and put key and val into map
+                2.2) get max freqNum
+                2.3) init a valid point as boolean. 
+                2.4) if cur point is not valid, remove the left ptr in map, and left ptr is incremented
+                2.5) get the longest len of str
         '''
+       
         left = 0
         freqMap = {}
-        maxFreq = 0
         strLen = 0
+        maxFreq = 0
         
         for right in range(len(s)):
-            freqMap[s[right]] = freqMap.get(s[right],0) + 1
-            
+            freqMap[s[right]] = freqMap.get(s[right], 0) + 1
             maxFreq = max(maxFreq, freqMap[s[right]])
             
-            isValid = (right - left + 1 - maxFreq <= k)
+            isValid = right - left + 1 - maxFreq <= k
             
             if not isValid:
                 freqMap[s[left]] -= 1
