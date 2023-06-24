1af1183b113f5091c123b33b189cd033624c3638
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-23 18:13:08 -0700

    Time: 43 ms (80.19%), Space: 16.3 MB (87.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -13,16 +13,19 @@ class Solution:
             1. init a stack
             2. traverse the input str to put each char into stack
                 2.1) check cur char is any open parentheses. if it is, put into stack
+                2.2) else if stack is not empty and top of stack is not equal to the curval, return false. else pop
+            3. return stack is empty as true
                 
         '''
-        stack, hm = [], {'(': ')', '{': '}', '[': ']'}
-        for ch in s:
-			# open bracket
-            if ch in '({[': 
-                stack.append(ch)
-			# close bracket
-            else: 
-                if not stack or hm[stack[-1]] != ch: return False
-                stack.pop()
-
+        stack = []
+        hm =  {'(' : ')', '{': '}', '[': ']'} 
+        
+        for i in s:
+            if i in hm:
+                stack.append(i)
+            else:
+                if not stack or hm[stack[-1]] != i:
+                    return False
+                else:
+                    stack.pop()
         return not stack
