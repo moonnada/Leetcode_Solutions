70a7db2f7ff28c288f466313da8f3b10806e1b60
moonnada <53587841+moonnada@users.noreply.github.com>
2023-06-29 14:08:03 -0700

    Time: 91 ms (12.56%), Space: 16.3 MB (96.04%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -12,17 +12,21 @@ class Solution:
                     2. non-negative
                     3. stored in reverse order
                     4. return the sum as a linked list
-                ex) l1 = [1,4,5,2], l2 = [2,9]
+                ex) l1 = [2,4,3,9], l2 = [5,6,4]
                 
-                1452
-                29
-                3362
+                2439
+                564
+                7089
                 
             P:
-                1. check egde cases(one node)
-                2. while both lists exist, add each node. if both sum > 9, then put 0 and add 1 to the next position
-                3. if one of list doesnt exist anymore, just put into the new list
-                4. reverse the new list
+                1. init a dummy list and set curr on dummy list
+                2. init a carry val
+                3. while l1 or l2 or carry
+                    3.1) init a val for checking each list exists. if not, put 0
+                    3.2) get carry and sum
+                    3.3) curr.next = remainder of the sum
+                    3.4) cur = cur.next
+                    3.5) check if each node has next node, then move. if not put none
                 
         '''
         
@@ -30,15 +34,16 @@ class Solution:
         dummy = ListNode(0)
         curr = dummy
         carry = 0
+        
         while l1 or l2 or carry:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
-            carry, out = divmod(val1 + val2 + carry, 10)
-                
+            carry, out = divmod(val1 + val2 + carry , 10)
+            
             curr.next = ListNode(out)
-            curr= curr.next
-                
+            curr = curr.next
+            
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-                
+            
         return dummy.next
