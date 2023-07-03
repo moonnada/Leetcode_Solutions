94ea64f96efd8b0a40bd9dfe96a727c9784ba852
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-02 22:57:25 -0700

    Time: 39 ms (93.00%), Space: 16.5 MB (16.66%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -11,17 +11,29 @@ class Solution:
             q) input tree can be null?
             
         M: binary
-            1. recursion
+            1. recursion (DFS)
         
         P: 
             1. check edge cases(null, one node)
-            2. while both
+            2. check cur values
+            3. recursion both sides
             
         '''
-        if not p and not q: return True
+#         if not p and not q: return True
         
-        if not p or not q: return False
+#         if not p or not q: return False
         
-        if p.val != q.val: return False
+#         if p.val != q.val: return False
         
-        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+#         return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+
+        que = deque([(p,q)])
+        while que:
+            p, q = que.popleft()
+            if p and q and p.val == q.val:
+                que.append((p.left, q.left))
+                que.append((p.right, q.right))
+                
+            elif p is not q:
+                return False
+        return True
