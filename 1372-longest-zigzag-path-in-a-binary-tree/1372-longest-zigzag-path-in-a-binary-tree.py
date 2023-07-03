541a6ea098b97079a9dd683ac7d1a272cb95ca39
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-03 13:59:23 -0700

    Time: 402 ms (52.33%), Space: 64.8 MB (13.01%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
@@ -7,32 +7,41 @@
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         '''
+        U:
+            key point
+            1. need to remember what the recent direction was
+            
         M: DFS , recursion
         
-        1. need to remember what the recent direction was
         P:
             1. check edge cases( null , one node)
-            2. make a helper func to visit recursively
-                2.1) check there is a left on the left or right side. if not, return cur depth
-                2.2) 
-    
+            2. return max of both sides of root
+            3. make a helper func to visit recursively
+                3.1) check cur root exists. if not, return cur depth
+                3.2) if cur direction is left, visit alternatively add depth + 1. AND THEN visit current direction with 0 depth
+                3.3) else, visit alternatively add depth + 1. AND THEN visit current direction with 0 depth
+                3.4 return depth    
         '''
+      
         if not root: return 0
         if not root.left and not root.right: return 0
         
-        return max(self.dfs(root.left, True, 0), self.dfs(root.right, False, 0))
+        return max( self.dfs(root.left, True, 0), self.dfs(root.right, False, 0))
     
     def dfs(self, root, isLeft, depth):
         if not root: return depth
-            
+        
         if isLeft:
-            depth = max( depth,
-                         self.dfs(root.right, False, depth+1),
-                        self.dfs(root.left, True, 0)
-                          )
+            depth = max(
+                depth,
+                self.dfs(root.right, False, depth+1),
+                self.dfs(root.left, True, 0)
+            )
         else:
-            depth = max( depth,
-                           self.dfs(root.left, True, depth + 1),
-                            self.dfs(root.right, False,0)
-                           )
+            depth = max(
+                depth,
+                self.dfs(root.left, True, depth+1),
+                self.dfs(root.right, False, 0)
+            )
+        
         return depth
