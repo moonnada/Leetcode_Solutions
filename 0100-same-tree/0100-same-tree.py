1a752ec776634d0ae0bbd61c5ed36901e2ce55f1
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-03 10:06:37 -0700

    Time: 53 ms (17.45%), Space: 16.5 MB (16.66%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -8,16 +8,14 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         '''
         U:
-            q) input tree can be null?
-            
-        M: binary
-            1. recursion (DFS)
+            q) input tree can be null? 
+        '''
         
-        P: 
+        '''
+            #DFS
             1. check edge cases(null, one node)
             2. check cur values
             3. recursion both sides
-            
         '''
 #         if not p and not q: return True
         
@@ -27,13 +25,29 @@ class Solution:
         
 #         return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
 
+        '''
+            #BFS
+            1. check edge cases(null, one node)
+            2. init a queue for both trees
+            3. while queue exists
+                3.1) pop a val from each queue
+                3.2) compare the values. if there are same, put each side's val into queue
+                3.3) if p and q are different nodes, then false
+            4. true
+        '''
+        
+        # if p and q: return True
+        # if not p or not q: return False
+        # if p.val != q.val: return False
+    
         que = deque([(p,q)])
         while que:
-            p, q = que.popleft()
+            p,q = que.popleft()
             if p and q and p.val == q.val:
                 que.append((p.left, q.left))
                 que.append((p.right, q.right))
-                
+            
             elif p is not q:
                 return False
+        
         return True
