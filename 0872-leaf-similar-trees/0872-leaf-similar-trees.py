5f24a2ddd09753417ea1bb02dbdb44f1a380c2c8
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-03 13:24:01 -0700

    Time: 49 ms (55.06%), Space: 16.4 MB (26.83%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -7,13 +7,20 @@
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         '''
+        M: recursion(dfs)
+        
         P:
+            1. make a func for recursion
+                1.1) if root not exist, return empty list
+                1.2) if root doesnt have one of sides, then put cur val into list
+                1.3) root has both a left and a right child, so the dfs func is called recursively and combine the result and return the val
+            2. check root1 and root2 lists are same
             
         '''
+        return self.dfs(root1) == self.dfs(root2)
+     
+    def dfs(self, root):
+        if not root: return []
+        if not (root.left or root.right): return [root.val]
+        return self.dfs(root.left) + self.dfs(root.right)
         
-        def dfs(root):
-            if not root: return []
-            if not (root.left or root.right): return [root.val]
-            return dfs(root.left) + dfs(root.right)
-        
-        return dfs(root1) == dfs(root2)
