d363ee7a43c8bc9098a76499394e5e37c5aaed09
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-04 13:36:10 -0700

    Time: 43 ms (82.12%), Space: 16.3 MB (73.56%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        '''
+        U:
+            q) root can be null?
+            q) can root have the same numbers multiple times?
+            
+        M: DFS (recursion)
+        
+        P: 
+            1. check edge cases( null, one node)
+            2. visit each side recursively
+            3. invert each side
+            4. return root
+        '''
+        
+        if not root: return None
+        
+        l = self.invertTree(root.right)
+        r = self.invertTree(root.left)
+        root.left = l
+        root.right = r
+        
+        
+        return root
+        
