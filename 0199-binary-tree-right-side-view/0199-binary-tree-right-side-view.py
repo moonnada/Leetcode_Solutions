a2b7dc2f5aa3b5154d0b00109ddb3848a4d47141
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-05 10:26:16 -0700

    Time: 51 ms (42.16%), Space: 16.4 MB (8.87%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -12,31 +12,60 @@ class Solution:
             
         M: BFS
         
-        p: 
-            1. check edge cases( null, one node, no right side)
-            2. init a list for ans
-            3. put cur node into the ans list
-            4. visit the right child recursively
-        '''
-        
-        # if not root or not (root.left and root.right) or not root.right: return root
-        
-        res = []
-        que = deque([root])
-        
-        while que:
-            rightSide = None
-            qLen = len(que)
-            
-            for i in range(qLen):
-                node = que.popleft()
-                if node:
-                    rightSide = node
-                    que.append(node.left)
-                    que.append(node.right)
-            
-            if rightSide:
-                res.append(rightSide.val)
+        p: (bfs)  
+            1. init a list for answer
+            2. init a queue with the root node
+            3. while queue exists
+                3.1) init a val to store the right side
+                3.2) get a length of cur level of queue
+                3.3) loop through the nodes in the cur level
+                    3.4) dequeue the next node from the queue
+                    3.5) if the node exists
+                        3.6) update the rightside val to store the cur node
+                        3.7) enqueue left and right side of the cur node
+                3.8) after processing all nodes in the cur level, if rightside is not null, then add the cur val to the ans list
                 
-        return res
+            
+            (dfs)
+            1. check edge cases
+            2. init a list for ans
+            3. make a helper func to visit recursively
+                3.1) if curLevel is eqaul to the len of the ans list, put the curVal into the list
+                3.2) iterative over the right and left children of the cur node
+                    3.3) if the chiild node exists, recursively call the helper func with the incremented level
+        '''
+                
+#         res = []
+#         que = deque([root])
         
+#         while que:
+#             rightSide = None
+#             qLen = len(que)
+            
+#             for i in range(qLen):
+#                 node = que.popleft()
+#                 if node:
+#                     rightSide = node
+#                     que.append(node.left)
+#                     que.append(node.right)
+            
+#             if rightSide:
+#                 res.append(rightSide.val)
+                
+#         return res
+
+        if not root: return []
+    
+        ans = []
+        
+        def helper(node, level):
+            if level == len(ans):
+                ans.append(node.val)
+                
+            for child in [node.right, node.left]:
+                if child:
+                    helper(child, level+1)
+                    
+        helper(root, 0)
+        
+        return ans
