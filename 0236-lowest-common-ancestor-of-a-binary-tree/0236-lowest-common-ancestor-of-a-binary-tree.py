0c9ef7d03322d0ad50f365db971455e7050440e5
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-04 17:39:56 -0700

    Time: 86 ms (57.70%), Space: 28.8 MB (20.94%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        '''
+        M: DFS (recursion)
+        
+        P: 
+            1. check edge cases(null, one node)
+            2. visit both sides recursively
+            3. if both sides exist, then just retun root
+            4. return one of side when exists
+        '''
+        #time: o(n), space: o(1) if not countring recursive, otherwise o(n)
+        
+#         if not root or root == p or root == q: return root
+        
+#         leftVal = self.lowestCommonAncestor(root.left, p,q)
+#         rightVal = self.lowestCommonAncestor(root.right, p,q)
+        
+#         if leftVal and rightVal:
+#             return root
+        
+#         return leftVal or rightVal
+
+        if not root or root == p or root == q: return root
+    
+        rightVal = self.lowestCommonAncestor(root.right, p, q)
+        leftVal = self.lowestCommonAncestor(root.left, p, q)
+        
+        if rightVal and leftVal: return root
+        
+        return rightVal or leftVal
+
+        
