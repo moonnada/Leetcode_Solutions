430c05a588823e8a5173005f1aa6077becb3cade
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-06 12:19:06 -0700

    Time: 60 ms (41.46%), Space: 17.6 MB (97.18%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -15,8 +15,9 @@ class Solution:
         
         P: 
             1. check edge case (null)
-            2. init a que and detph as 1
+            2. init a que and detph as 0
             3. while que
+                3.0) loop a cur level of children
                 3.1) curNode is popped from que
                 3.2) if curnode.left, add the node into queue
                 3.3) if curnode.right, add the node into queue
@@ -28,18 +29,15 @@ class Solution:
         depth = 0
         que = deque([root])
         
-        while que: 
-            
+        while que:
             for _ in range(len(que)):
                 curNode = que.popleft()
-
-
                 if curNode.left:
                     que.append(curNode.left)
                     
                 if curNode.right:
                     que.append(curNode.right)
             
-            depth +=1
+            depth += 1
             
         return depth
