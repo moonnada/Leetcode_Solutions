d31206c78cb84425a9414d9f3d20eca650eb3542
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-13 15:17:16 -0700

    Time: 55 ms (63.42%), Space: 17.1 MB (38.81%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        '''
+        U:
+            q) node val is only integer?
+            
+        M: BFS
+        
+        P:
+            1. check edge case(null)
+            2. init an ans list and queue
+            3. while que exists
+                3.1) pop cur node from queue
+                3.2) visit cur level of node by using a loop    
+                    3.3) put cur node into queue and list
+            4. return list        
+            
+        '''
+        if not root: return []
+        ans = []
+        que = deque([root])
+        level = 0
+        
+        while que:
+            ans.append([])
+            for _ in range(len(que)):
+                curNode = que.popleft()
+                ans[level].append(curNode.val)
+                
+                if curNode.left:
+                    que.append(curNode.left)
+                if curNode.right:
+                    que.append(curNode.right)
+                    
+            level+=1
+            
+        return ans
+        
