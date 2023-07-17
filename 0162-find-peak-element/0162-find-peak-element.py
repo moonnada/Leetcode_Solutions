1eeec4ab805dba57c207d1bfbed4cad94e304167
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-17 16:21:21 -0700

    Time: 60 ms (65.94%), Space: 16.3 MB (93.57%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -9,19 +9,26 @@ class Solution:
             
         M: binary search
         
+        P: 
+            1. init two ptrs as left and right
+            2. while left <= right,
+                2.1) check the left neighbor is greater. if it is, the right val is moved to the left side
+                2.2) check the right neighbor is greater. if it is, the left val is moved to the right side
+                2.3) else return the mid index ()
+                
+            
         
             
         '''
-        left , right = 0, len(nums) -1
+        left, right = 0, len(nums) -1
         
         while left <= right:
             mid = (left + right) // 2
-            #left neighbor greater
-            if mid > 0 and nums[mid] < nums[mid-1]: 
-                right = mid -1
             
-            #right neighbor greater
-            elif mid<len(nums)-1 and nums[mid] < nums[mid+1]:
-                left = mid + 1
+            if nums[mid] < nums[mid-1] and mid > 0:
+                right = mid-1
+            
+            elif mid < len(nums)-1 and nums[mid] < nums[mid+1]:
+                left = mid+1
             
             else: return mid
