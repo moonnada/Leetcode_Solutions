8a4b68ee4243fc9d90674005d9687f423beb823a
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-17 16:14:02 -0700

    Time: 66 ms (29.10%), Space: 16.3 MB (99.52%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -7,15 +7,21 @@ class Solution:
             - cant have the same numbers continuously
             - must write in o(lgn) time.
             
-            q) 
+        M: binary search
+        
+        
             
         '''
         left , right = 0, len(nums) -1
         
-        while left < right:
+        while left <= right:
             mid = (left + right) // 2
-            if nums[mid] > nums[mid+1]:
-                right = mid
-            else: left = mid + 1
-                
-        return left
+            #left neighbor greater
+            if mid > 0 and nums[mid] < nums[mid-1]: 
+                right = mid -1
+            
+            #right neighbor greater
+            elif mid<len(nums)-1 and nums[mid] < nums[mid+1]:
+                left = mid + 1
+            
+            else: return mid
