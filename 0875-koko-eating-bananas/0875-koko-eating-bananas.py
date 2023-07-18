16028d49a2f35653e4d0d024faf3c0f6381d1622
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-18 10:44:09 -0700

    Time: 472 ms (64.59%), Space: 18 MB (10.81%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -2,12 +2,24 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         '''
         U:
-            keypoints: return the minimun number 
-            how many bananas should she eat per hour
+            keypoints: 
+                - return the minimum number 
+                - how many bananas could she eat per hour
             
             ex) [30, 11, 23, 4, 20] , h=6
-                 
-                 
+            
+                1  2 3....   29 30 
+                
+                M: binary search
+        
+        P:
+            1. init two ptrs as left and right 
+            2. while left <= right,
+                2.1) get the mid point
+                2.2) init a hours val
+                2.3) traverse the input list to get valid hours
+                    2.4) if hours > target, update the left point
+                    2.5) else, update the right point              
         '''
         left, right = 1, max(piles)
         
