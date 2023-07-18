2f5892091962d67768c4ea1273b560730acbf465
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-17 19:32:33 -0700

    Time: 61 ms (61.17%), Space: 16.5 MB (33.97%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -2,10 +2,13 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         '''
         U:
+            keypoints:
             - the input arr contains multiple peeks, return the index to any of the peaks.
             - the input list can have negative values
             - cant have the same numbers continuously
             - must write in o(lgn) time.
+            
+            ex) [9,2,1] => return 0
             
         M: binary search
         
@@ -16,15 +19,12 @@ class Solution:
                 2.2) check the right neighbor is greater. if it is, the left val is moved to the right side
                 2.3) else return the mid index ()
                 
-            
-        
-            
         '''
         left, right = 0, len(nums) -1
         
         while left <= right:
-            mid = (left + right) // 2
-            
+            # mid = (left + right) // 2
+            mid = left + (right-left) // 2
             if nums[mid] < nums[mid-1] and mid > 0:
                 right = mid-1
             
