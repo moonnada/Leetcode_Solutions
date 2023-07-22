7159b9dc48f3d20fe11f1c6726d742fa3a2a5bb4
moonnada <53587841+moonnada@users.noreply.github.com>
2023-07-21 23:01:20 -0700

    Time: 85 ms (93.54%), Space: 22.9 MB (96.48%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -8,7 +8,7 @@ class Solution:
             2. iterative until n+1 to convert binary numbers
         '''
         
-        ans = [0] * (n+1)
+        ans = [0]
         for x in range(1, n+1):
-            ans[x] = ans[x & (x-1)] + 1
+            ans.append(ans[x>>1] + x%2)
         return ans
