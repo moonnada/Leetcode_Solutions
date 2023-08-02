8fb2781d7fd20ddd8cd010b7c8fef11d52098cf5
moonnada <53587841+moonnada@users.noreply.github.com>
2023-08-01 23:04:58 -0700

    Time: 138 ms (93.73%), Space: 96.7 MB (29.66%) - LeetHub

---
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+    /*
+    U:
+        q) root can be null?
+    
+    M: dfs (recursion)
+    
+    P: 
+        1. check edge cases(null)
+        2. init a new func to visit recursively
+            
+            2.1) if curnode.val >= low and curNode.val <= high, then add the val into the ans val
+            2.2) if curNode.val > low, visit left
+            2.2) if curNode.val < h, visit right
+        3. return sum
+    */
+    
+    let sum = 0
+    const dfs = (node) => {
+        if(node.val >= low && node.val <= high) sum += node.val;
+        
+        if(node.val > low && node.left !== null) dfs(node.left);
+        
+        if(node.val < high && node.right !== null) dfs(node.right);
+    }
+        
+    dfs(root)
+    
+    
+    return sum
+};
