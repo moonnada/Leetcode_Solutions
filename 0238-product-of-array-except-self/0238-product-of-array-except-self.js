b1f841f0d30f1a88ae1588669fd0eab394d99f9d
moonnada <53587841+moonnada@users.noreply.github.com>
2023-08-04 22:02:03 -0700

    Time: 83 ms (96.53%), Space: 56.1 MB (18.24%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,20 +3,36 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let leftArr = [];
-    let rightArr = [];
-    let leftMulti = 1;
-    let rightMulti = 1;
+//     let leftArr = [];
+//     let rightArr = [];
+//     let leftMulti = 1;
+//     let rightMulti = 1;
+    
+//     for(let i=0; i<nums.length; i++){
+//         leftArr[i] = leftMulti;
+//         leftMulti *= nums[i];
+//     }
+    
+//     for(let i=nums.length-1; i>=0; i--){
+//         rightArr[i] = rightMulti;
+//         rightMulti *= nums[i];
+//         rightArr[i] *= leftArr[i]
+//     }
+//     return rightArr;
+    
+    let ans = [];
+    let multi = 1;
     
     for(let i=0; i<nums.length; i++){
-        leftArr[i] = leftMulti;
-        leftMulti *= nums[i];
+        ans[i] = multi;
+        multi *= nums[i];
     }
     
+    multi = 1;
     for(let i=nums.length-1; i>=0; i--){
-        rightArr[i] = rightMulti;
-        rightMulti *= nums[i];
-        rightArr[i] *= leftArr[i]
+        ans[i] *= multi;
+        multi *= nums[i];
+        
     }
-    return rightArr;
+    return ans;
 };
