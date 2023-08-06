650f932e9bb678089edf5fb0dc627e099460eb1a
moonnada <53587841+moonnada@users.noreply.github.com>
2023-08-05 18:19:56 -0700

    Time: 55 ms (97.48%), Space: 44.2 MB (87.63%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,15 +3,31 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    /*
+    - remove non-alphabenumric. only contain number and lowercase.
+    - empty str => true
+    
+    1. check edge cases(null, one length)
+    2. init two ptrs(left and right)
+    3. convert the input str to contain numbers and lowercases only
+    4. while left <= right
+        3.1) if left element and right element are same, left++, right--
+        3.2) else return false
+    5. return true
+    */
+    
+    if(s.length === 0 || s.length ===1) return true;
     s = s.toLowerCase();
     s = s.replace(/[^A-Za-z0-9]/g, '');
-    
     let left = 0;
     let right = s.length-1;
-    
-    while(left<= right){
-        if(s[left] !== s[right]) return false;
-        left++; right--;
+    while(left <= right){
+        if(s.charAt(left) === s.charAt(right)){
+            left++;
+            right--;
+        } else {
+            return false;
+        }
     }
     return true
-};
+}
