9bc19ab62a804632e37e159c5e5a4630ec4a7128
moonnada <53587841+moonnada@users.noreply.github.com>
2023-08-06 17:02:36 -0700

    Time: 71 ms (90.67%), Space: 52 MB (14.84%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    /*
-    1. traverse the input arr
-    2. get min val between (i and i+1)
-    3. if n[i+1] > n[i], calculate profit.
-    4. set profit as max if new profit is larger
-    */
-    
-    let max = 0;
     let min = prices[0];
+    let max = 0;
     
     for(let i=1; i<prices.length; i++){
         min = Math.min(min, prices[i]);
-        max = Math.max(max, prices[i]-min)      
+        max = Math.max(max, prices[i]-min)
     }
     return max
 };
