af03290f9363f90e4f95c60463590abcc87857ed
moonnada <53587841+moonnada@users.noreply.github.com>
2023-08-06 22:58:45 -0700

    Time: 83 ms (70.33%), Space: 45.9 MB (83.63%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -4,23 +4,37 @@
  */
 var lengthOfLongestSubstring = function(s) {
     /*
-    1. init a set
-    2. init a end and max as 0
-    3. traverse the input string to get longest
-        3-1) while set has the cur char, delete the char in the set, and left is incremented
-        3-2) set has not the cur char, put the char in the set and get max
+    ex) s = "dvdfadbukh"
+    
+           i       c
+    set: d,v,d,f,a,d
+    len: 4
+    max: 4
+    
+    P:
+        1. check edge cases
+        2. init a set and max val as 0
+        3. traverse the input str to find the longest substr
+            3.1) while set has the cur char in the set
+                    left++, remove the char in the set\\
+            3.2) put the char into set
+            3.3) get the max len
     */
     
     let set = new Set();
     let max = 0;
     let left = 0;
+    
     for(let i=0; i<s.length; i++){
         while(set.has(s.charAt(i))){
-            set.delete(s.charAt(left))
-            left+= 1;
+            set.delete(s.charAt(left));
+            left++;
         } 
         set.add(s.charAt(i));
-        max = Math.max(max, i-left + 1);
+        max = Math.max(max, i-left+1);
     }
     return max
-};
+ 
+    
+}
+    
