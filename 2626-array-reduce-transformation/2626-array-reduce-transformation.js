eac5ba72aa56fac0dc787444bd72fb77c425d82b
moonnada <53587841+moonnada@users.noreply.github.com>
2023-08-13 20:58:51 -0700

    Time: 44 ms (98.10%), Space: 43.6 MB (14.24%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let ans = init;
+    if(nums.length === 0) return init;
+    for(const element of nums){
+        ans = fn(ans, element);
+    }
+    return ans
+    
+};
