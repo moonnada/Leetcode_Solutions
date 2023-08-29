8f5c660d7d1c167f36e641d692bbb078f26b6c21
moonnada <53587841+moonnada@users.noreply.github.com>
2023-08-28 20:27:40 -0700

    Time: 50 ms (87.77%), Space: 43.9 MB (61.67%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    const ans = [];
+    let word = "";
+    
+    for(let i=0; i<s.length; i++){
+        if(s.charAt(i) === ' '){
+            word && ans.unshift(word);
+            word = "";
+        } else {
+            word += s.charAt(i)
+        }
+    }
+    
+    word && ans.unshift(word);
+    return ans.join(' ')
+};
