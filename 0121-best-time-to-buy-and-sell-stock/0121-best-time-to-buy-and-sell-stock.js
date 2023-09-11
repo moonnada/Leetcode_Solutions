3dc3fedb3bd3c6476594abd4b68ecd258f1f6f0a
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-11 15:14:50 -0700

    Time: 72 ms (65.36%), Space: 51.6 MB (43.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -7,23 +7,19 @@ var maxProfit = function(prices) {
     /*
     ex) 
     prices = [7,1,5,3,6,4]
-                  c 
-                p
+                  
                 
-    maxProfit=4
-    
-    1. init a val as starting on arr[0]
-    2. traverse the input arr to get the max profit
-        3. check edge case which is arr[0] > arr[i] continously
-        4. compare and get the max profit
+                
+   min = 1
+   max = max(prices[i] - min) = 4
     
     */
     
-    let min = prices[0];
+   let min = prices[0];
     let max = 0;
     for(let i=1; i<prices.length; i++){
         min = Math.min(min, prices[i]);
-        max = Math.max(max, prices[i]-min)
+        max = Math.max(max, prices[i] - min)
     }
     return max
     
