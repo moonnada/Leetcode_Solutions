18f0711af0a59ebbd8d2082df084ed86b8615a75
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-13 13:28:02 -0700

    Time: 72 ms (64.05%), Space: 44.4 MB (56.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,25 +5,21 @@
  */
 var isAnagram = function(s, t) {
     /*
-    map
+    s = "anagram", t = "nagaram"
     
-    1. init a map (key: char, value: cnt)
-    2. chech edge cases
-    3. iterative the s str to put key and value
-    4. iterative the t str to compare each key and value. if find different, return false.
-    5. return true
+    1. compare both str length. if they are different, then return false
+    
     */
-    
     if(s.length !== t.length) return false;
-    let table = new Array(26).fill(0);
     
-    for(let i=0; i<s.length; i++){
-        table[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    const map = {};
+    
+    for(let c of s){
+        map[c] = (map[c] || 0) + 1;
     }
     
-    for(let i=0; i<t.length; i++){
-        table[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
-        if(table[t.charCodeAt(i) -'a'.charCodeAt(0)]  < 0) return false;
-    }
-    return true
+    for(let c of t){
+        if(!map[c]) return false;
+        map[c]--;
+    } return true
 };
