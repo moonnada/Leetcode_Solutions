fbc293f22cf94a506e880a1619c68d7c686ca60e
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-12 20:36:01 -0700

    Time: 46 ms (99.77%), Space: 49.9 MB (31.00%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -4,31 +4,25 @@
  */
 var maxArea = function(height) {
     /*
-    1. init two ptrs (left and right)
-    2. while( left < right)
-        2.1) get height. smaller one, then moving to the next
-        2.2) get width
-        2.3) get area
-        2.4) get max area
-        
-    time: o(n), space: o(1)
-    */
+              0 1 2 3 4 5 6 7 8
+    height = [1,8,6,2,5,4,8,3,7]
+              L               R
+    get width and height 
     
+    */
     let left = 0;
     let right = height.length-1;
-    let area = 0;
     let max = 0;
-    
     while(left < right){
         let width = right - left;
-        let heightVal = Math.min(height[left], height[right]);
-        area = width * heightVal;
-        max = Math.max(max, area);
-        if(height[left] < height[right]) left++;
-        else right--;
+        let validHeight = Math.min(height[left], height[right]);
+        if(height[left] >= height[right]){
+            right--;
+        }else {
+            left++;
+        }
+        max = Math.max(max, width*validHeight);
+        
     }
-    return max;
-    
-    
-    
+    return max
 };
