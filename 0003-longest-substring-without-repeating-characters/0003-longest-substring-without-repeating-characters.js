ea0a51bf8ce0cde745463fc43e554b2a4a0ddb50
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-17 13:58:45 -0700

    Time: 66 ms (87.56%), Space: 46.7 MB (71.45%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,19 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    /*
-    
-    */
-    
     let set = new Set();
-    let max = 0;
     let left = 0;
+    let max = 0;
+    
     for(let i=0; i<s.length; i++){
         while(set.has(s[i])){
             set.delete(s[left]);
             left++;
         }
-        set.add(s[i])
+        set.add(s[i]);
         max = Math.max(max, i-left+1)
     }
     return max
