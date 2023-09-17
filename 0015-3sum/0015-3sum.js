8d85399e05818fd4a95fdc6d4c95655cdd869b70
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-17 14:16:44 -0700

    Time: 152 ms (66.24%), Space: 58.1 MB (92.34%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -4,36 +4,37 @@
  */
 var threeSum = function(nums) {
     /*
-    ex)
-     nums = [-1,0,1,2,-1,-4]
-     
-     1. sort.   nums = [-4,-1,-1,0,1,2]
-     2. init an arr for answer
-     3. init two pointer
+            nums = [-1,0,1,2,-1,-4]
+    sort [-4,-1,-1,0,1,2]
+    
+    1. sort the input arr
+    2. traverse the arr from 0 to len -2
+        2.0) check edge cases to avoid duplication
+        2.1) init two pointers
     */
     
-   nums.sort((a,b) => a-b);
-    let ans= [];
+    nums.sort((a,b) => a-b);
+    let ans = [];
     for(let i=0; i<nums.length-2; i++){
         if(i>0 && nums[i] === nums[i-1]) continue;
+        
         let mid = i+1;
         let end = nums.length-1;
         
-        while(mid <end){
-            let sum = nums[mid] + nums[i] + nums[end];
-            if(sum === 0){
-                ans.push([nums[i], nums[mid], nums[end]]);
+        while(mid < end){
+            let sum = nums[i] + nums[mid] + nums[end];
+            if(sum === 0 ){
+                ans.push([nums[i], nums[mid], nums[end]])
                 
                 while(nums[mid] === nums[mid+1]) mid++;
                 while(nums[end] === nums[end-1]) end--;
                 
                 mid++;
                 end--;
-            }else {
-                
-            
-            sum > 0 ? end-- : mid++;
-        }}
+            } else {
+                sum > 0 ? end-- : mid++;
+            }
+        }
     }
     return ans
 };
