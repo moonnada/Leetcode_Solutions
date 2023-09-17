4c66ef011b2d1198d9990b4c4604a28caa7b2269
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-17 13:23:45 -0700

    Time: 90 ms (90.88%), Space: 62.3 MB (73.60%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -4,38 +4,24 @@
  */
 var longestConsecutive = function(nums) {
     /*
-    U:
-        - unsorted integer arr
-        - return the len of the longest consecutive elements
-        - must run in o(n) time
-        q) input elements are all distinct?
-        
-    ex) nums = [100,4,200,1,3,2] 
+    run in o(n) time.
     
-    set: 100,4,200,1,3,2
-    
-    1. init a set which has input arr
-    2. init a count and max as 0
-    3. traverse the set to find consecutive elements
-        3.1) if set not has n-1, cnt=0
-        3.2) while set has n, cnt and n are incremented. get a new max
-    4. return max
+    1. init a set and put the input nums into the set
+    2. traverse the set in find the consecutive numbers
+        2.1) while (set.has(n-1)), cnt++. max = math.max(cnt, max)
     */
     
-    let set = new Set(nums);
-    let cnt = 0;
+   let set = new Set(nums);
     let max = 0;
-    
     for(let n of set){
         if(!set.has(n-1)){
-            cnt = 0;
+            let cnt = 0;
             while(set.has(n)){
-                n++;
                 cnt++;
-                max = Math.max(cnt, max);
+                n++;
+                max = Math.max(max, cnt)
             }
         }
     }
     return max
-    
 };
