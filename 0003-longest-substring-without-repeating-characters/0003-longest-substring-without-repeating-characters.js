0f9025c8cdf927e1e23c981c8cb98089958970d9
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-17 13:53:14 -0700

    Time: 61 ms (94.88%), Space: 46.7 MB (71.45%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -4,21 +4,19 @@
  */
 var lengthOfLongestSubstring = function(s) {
     /*
-    s = "abcabcbb"
     
     */
+    
     let set = new Set();
     let max = 0;
     let left = 0;
     for(let i=0; i<s.length; i++){
-        while(set.has(s.charAt(i))){
-            set.delete(s.charAt(left));
+        while(set.has(s[i])){
+            set.delete(s[left]);
             left++;
-        } 
-        set.add(s.charAt(i))
-        
-         max = Math.max(max, i-left+1)
+        }
+        set.add(s[i])
+        max = Math.max(max, i-left+1)
     }
     return max
-   
 };
