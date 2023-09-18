83e2f519ebb2e21e804c5b513bfa89194f988337
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-18 09:41:06 -0700

    Time: 77 ms (51.49%), Space: 42.8 MB (56.95%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,29 +4,19 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    /*
-    s = "AABABBA", k = 1
-    
-    => ans = 4
-    
-    sliding window 
-    */
- 
-    let left = 0;
-    let right = 0;
-    let map = new Map();
+    let left =0;
+    let right = 0; 
     let max = 0;
+    let map = new Map();
     
     while(right < s.length){
-        let curChar = s[right];
-        map.set(curChar, (map.get(curChar) || 0) + 1 );
+        map.set(s[right], (map.get(s[right]) || 0) +1);
         
-        max = Math.max(map.get(curChar), max)
-        
+        max = Math.max(max, map.get(s[right]));
         if(right-left+1 - max > k){
-            map.set(s[left], map.get(s[left]) -1);
+            map.set(s[left], map.get(s[left])-1);
             left++;
-        }  right ++;
-        
-    } return right - left
-}
+        } right++;
+    }
+    return right - left
+};
