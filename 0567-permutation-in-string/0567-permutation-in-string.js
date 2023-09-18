87029359da28a3e444bf64243b4cd5cf6e0b995e
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-18 15:31:16 -0700

    Time: 66 ms (82.30%), Space: 45.7 MB (54.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -4,31 +4,42 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
+  /*
+   s1 = "ab", s2 = "eidbaooo"
+   rs1 = 'ba'
+   1. if s1.length > s2.length, return false
+   2. reverse s1
+   
+  */
+
     if(s1.length > s2.length) return false;
-    let needed = {};
-    for(let i=0; i<s1.length; i++){
-        needed[s1[i]] = needed[s1[i]] ? needed[s1[i]]+ 1 : 1
-    };
     
-    let left=0, right=0, requiredLength = s1.length;
+    let needed = new Map();
+    for(let i=0; i<s1.length; i++){
+        needed[s1[i]] = needed[s1[i]] ? needed[s1[i]]+1 : 1;
+    }
+    
+    let left = 0;
+    let right = 0;
+    let requiredLength = s1.length;
     
     while(right < s2.length){
-        if(needed[s2[right]] > 0) {
+        if(needed[s2[right]] > 0){
             requiredLength--;
         }
-        if(requiredLength === 0) return true;
+        
+        if(requiredLength=== 0) return true;
         
         needed[s2[right]]--;
         right++;
         
-        
-        
         if(right-left === s1.length){
-            if(needed[s2[left]] >=0) { 
+            if(needed[s2[left]] >= 0){
                 requiredLength++;
             }
             needed[s2[left]]++;
-            left++;
+            left++
         }
-    } return false
+    }
+    return false
 };
