6809cb562cb3afc21e31f2d9bfb7167d043b81fd
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-09-22 16:26:15 -0700

    Time: 60 ms (88.07%), Space: 44.8 MB (87.25%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.js
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -5,35 +5,24 @@
  */
 var minEatingSpeed = function(piles, h) {
     /*
-    ex)
-    [30,11,23,4,20] , h = 6.
+    [3,6,7,11], h = 8
+    1  2 2  3
+    kbanana = nums[i] / 
+    => 4
     
-    Binary Search.  //time : O(nlgm). n be the length of the input array, m be the maximum number of bananas
-    1. init start and end val. ex) start = 1, end = 30
-    2. while start <= end
-    2-1) get mid val between start and end. ex) (1+30)/2 = 15
-            init a hrs val. 
-    2-2) iterate a loop to get hrs by using the ceil() function. hrs += ceil(each pile / mid)
-    2-3) if hrs > h, end = mid -1, else start = mid +1
+    [30,11,23,4,20], h = 6
     */
     
-   /*
-    1. set up start and end val
-    2. while start < end 
-        2-1. init a mid val
-        2-2. init hr as 0
-        2-3. traverse a loop to get Math.celi(element / mid )
-        2-4. if hr > h, start = mid +1, else end = mid-1
-   */
     let start = 0;
     let end = Math.max(...piles);
-    while( start <= end){
+    while(start <= end){
         let mid = Math.floor((start + end) / 2);
         let hr = 0;
         for(let pile of piles){
             hr += Math.ceil(pile / mid);
         }
-        if(hr > h) start = mid +1;
+        
+        if(hr > h) start = mid+1;
         else end = mid -1;
     }
     return start
