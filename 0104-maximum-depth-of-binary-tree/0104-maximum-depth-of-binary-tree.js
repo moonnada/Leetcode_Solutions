53260fe0d6c2902c71a26b408d429be7d332b38c
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-11-06 14:17:47 -0800

    Time: 60 ms (52.82%), Space: 45 MB (71.57%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -18,10 +18,10 @@ var maxDepth = function(root) {
     3. get the max depth to compare 
     */
     
-    if(!root) return 0;
+    if(!root) return 0
+    
     let left = maxDepth(root.left);
     let right = maxDepth(root.right);
     
     return Math.max(left, right) + 1
-    
 };
