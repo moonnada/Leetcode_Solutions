4e7337f6e838386edf0b56f0ca6e2370e2b18e19
Ray Kim <53587841+moonnada@users.noreply.github.com>
2023-11-09 17:13:43 -0800

    Time: 71 ms (63.93%), Space: 48.9 MB (82.55%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    /*
+    dfs
+    */
+     if(!root) return false;
+    if(areSame(root, subRoot) || !subRoot) return true
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+    
+    function areSame(t1,t2){
+        if(!t1 && !t2) return true
+        if(!t1 || !t2) return false
+        if(t1.val === t2.val){
+            return areSame(t1.left, t2.left) && areSame(t1.right, t2.right)
+        }
+    }
+//     if(!root) return false;
+//     if(areSame(root, subRoot) || !subRoot) return true;
+//     return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    
+//     function areSame(t1,t2){
+//         if(!t1 && !t2) return true;
+//         if(!t1 || !t1) return false;
+//         if(t1.val === t2.val) {
+//             return areSame(t1.left, t2.left) && areSame(t1.right, t2.right)
+//         }
+//     }
+};
