8e7a38466e54974d0c5a4167eb85b6d6b6cbb57f
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-01-30 14:43:27 -0800

    Time: 30 ms (99.81%), Space: 17 MB (65.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,38 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+# #       0. check edge case  
 #         1. init a dict
+#         2. traverse to put the 's' element into the dict
+#         3. traverse to decrement the 't'element in the dict
+#         4. iterative the dict to compare dicts value
         
 
-        if len(s) != len(t): return False
+#         if len(s) != len(t): return False
         
-        count = defaultdict(int)
+#         count = defaultdict(int)
+        
+#         for i in s:
+#             count[i] += 1
+            
+#         for i in t:
+#             count[i] -= 1
+            
+#         for val in count.values():
+#             if val != 0:
+#                 return False
+
+        count = {}
         
         for i in s:
-            count[i] += 1
+            count[i] = count.get(i,0) + 1
             
         for i in t:
-            count[i] -= 1
+            count[i] = count.get(i,0 )-1
             
-        for val in count.values():
-            if val != 0:
-                return False
+        for i in count.values():
+            if i != 0: return False
+            
+    
             
         return True
             
