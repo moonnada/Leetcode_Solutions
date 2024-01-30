3b45ef36e67af6b7cf77eb25ee032103c90d56c6
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-01-30 14:48:47 -0800

    Time: 56 ms (86.13%), Space: 17.8 MB (64.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,22 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        '''
-        U: 
-            q) input arr can be empty?
-            q) input arr is sorted?
-
-        M:
-            hashmap (key: indice , val: element)
-        P:
-            1. init a map
-            2. traverse the input arr to put key and val inside a map
-                2.1) if target - curVal is in the map, then return the cur Indice and map's indice
-                2.2) else put cur val and key inside a map
-        '''
-        
-        hashmap = {}
+        myMap = {}
         for i in range(len(nums)):
             lookFor = target - nums[i]
-            if lookFor in hashmap:
-                return [i, hashmap[lookFor]]
-            hashmap[nums[i]] = i
+            if lookFor in myMap:
+                return [i, myMap[lookFor]]
+            myMap[nums[i]] = i
+            
+            
+#         map (key: idx, val: element)
+#   1. traverse the input arr to put key and value
+#       1.1) if map has target - cur element, return cur index and map's index
+        
+    
+        
