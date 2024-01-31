d13197b72e769aedebc174e2a3879c32c4679931
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-01-31 11:53:33 -0800

    Time: 58 ms (30.82%), Space: 16.9 MB (65.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,11 +6,9 @@ class Solution:
         
         for i in range(len(s)):
             countS[s[i]] = countS.get(s[i], 0) + 1
-            countT[t[i]] = countT.get(t[i], 0 ) + 1
-        
+            countT[t[i]] = countT.get(t[i],0) + 1
+            
         for i in countS:
-            if countS[i] != countT.get(i,0): return False
+            if countS[i] != countT.get(i, 0): return False
             
         return True
-        
-        
