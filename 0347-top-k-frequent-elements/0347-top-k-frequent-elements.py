166fce37d5dfaa928f526cab57de1f7cca8f3ee6
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-01-31 15:27:31 -0800

    Time: 87 ms (81.32%), Space: 21.9 MB (54.39%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,27 +2,34 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
 #         q) is the input list sorted?
-        
-#         1. init a hashmap(key: element, value: count of its element)
-#         2. traverse the input list to put value in the map
-#             2.1) add the cur element into map
-#             2.2) if count of cur element >= k, add into ans list 
-#         3.return ans list
-        
+#             hashmap (key: cnt, value: elements)
+    
+#         0. init a hashmap and freq list
+#         1. traverse the input list to put key and value into the map
+#         2. traverse the hashmap to put each element and count into the freq list
+#         3. traverse the freq from end to start
+#             3.1) iterative a number in the freq to put elements into ans list
+#             3.2) if len of ans list == k, return ans
+
+
         count = {}
-        freq = [[] for i in range(len(nums)+1)]
+        freq = [[] for i in range(len(nums) + 1)]
         
-        for i in nums:
-            count[i] = count.get(i,0) + 1
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
             
         for num, cnt in count.items():
             freq[cnt].append(num)
             
         res = []
         
-        for i in range(len(freq)-1, 0, -1):
+        for i in range(len(freq)-1, 0 ,-1):
             for num in freq[i]:
+                
                 res.append(num)
                 
-                if len(res) == k:
-                    return res
+                if len(res) == k: return res
+        
+        
+        
+    
