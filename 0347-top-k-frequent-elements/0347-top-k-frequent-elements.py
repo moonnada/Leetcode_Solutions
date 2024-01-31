9e5a1ac4642e6c161a7a1b0dbf01695eb7281955
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-01-31 15:06:26 -0800

    Time: 97 ms (42.69%), Space: 22 MB (43.50%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,63 +1,28 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        '''
-        U:
-            q) input arr can be empty?
-            q) what should i return if there is the same count but k is not sufficient
-            
-        num: [1,1,1,2,2,3] k=2 => [1,2]
         
-        M : 
-            hashmap (key: count, val: number), bucketsort
-            
-        P :
-            1. init a map and a bucket
-            2. iterate a loop to put key and val inside the map
-            3. iterate a loop to put number and its count from map to bucket
-            4. init a result arr
-            5. traverse the bucket from at the end to start
-                5.1. make a loop to put result arr
-                5.2. if the result arr's lentgh is same as k, then just return the arr
-        '''
+#         q) is the input list sorted?
+        
+#         1. init a hashmap(key: element, value: count of its element)
+#         2. traverse the input list to put value in the map
+#             2.1) add the cur element into map
+#             2.2) if count of cur element >= k, add into ans list 
+#         3.return ans list
         
         count = {}
-        freq = [[] for i in range(len(nums) + 1)]
+        freq = [[] for i in range(len(nums)+1)]
         
         for i in nums:
-            count[i] = count.get(i , 0) + 1
+            count[i] = count.get(i,0) + 1
             
         for num, cnt in count.items():
             freq[cnt].append(num)
             
         res = []
         
-        for i in range(len(freq)-1, 0 ,-1):
+        for i in range(len(freq)-1, 0, -1):
             for num in freq[i]:
                 res.append(num)
                 
                 if len(res) == k:
                     return res
-        
-        
-        
-        
-        
-        
-        
-#         count = {}  # hashmap (key: count, val: number)
-#         freq = [[] for i in range(len(nums) + 1)] # bucket sort. init a bucket
-        
-#         for n in nums: #put key and count into map
-#             count[n] = count.get(n,0) + 1
-            
-#         for n, cnt in count.items(): #add number based on count as a key
-#             freq[cnt].append(n)
-            
-#         res = []
-#         for i in range(len(freq)-1, 0, -1):
-#             for n in freq[i]:
-#                 res.append(n)
-#                 if len(res) == k:
-#                     return res
-
-        
