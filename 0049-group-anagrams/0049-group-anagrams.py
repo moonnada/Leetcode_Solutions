d6b0ccd241eb77b58929c882ba0b671f8ddbd4b7
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-01 13:16:59 -0800

    Time: 91 ms (64.16%), Space: 19.3 MB (99.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,27 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-    
-#         q) can have the same letters consecutively?
-#         asd
-        
-#         0. init a hashmap (key: a word, value: array of the anagram words)
-#         1. traverse the input arr to make group anagrams
-#             1.1) sort the cur word
-#             1.2) if map has the cur sorted word, then put its array
-#             1.2) if not, then put the word as a new key and value in the map
-#         2. return the map's value
-
-        # time = o(m * nlgn)
-        # space = o(n)
-        
-        anagrams = {}
+        newMap = {}
         for word in strs:
             temp = ''.join(sorted(word))
-            
-            if temp in anagrams:
-                anagrams[temp].append(word)
+            if temp in newMap:
+                newMap[temp].append(word)
             else:
-                anagrams[temp] = [word]
-                
-        return list(anagrams.values())
+                newMap[temp] = [word]
+        
+        return list(newMap.values())
