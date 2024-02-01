e9490006d06b036809ebcf2308d4f68df489bc46
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-01 15:17:46 -0800

    Time: 345 ms (84.52%), Space: 31.9 MB (68.97%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,34 +1,31 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         '''
-        U:
-            q) can input arr be empty?
-            
-        M:
-            set
-            
-        P:
-            1. init a set and count val
-            2. iterate a loop to find a val in the set
-                2.1) if a val -1 is not in set, then curVal would be a new current value
-                2.2) set count val as 1
-                2.3) while val + 1 in set, keep incrementing curval and count
-                2.4) out of while loop, get a max count
-            
+        Input: nums = [100,4,200,1,3,2]
+        Output: 4
+        
+        M: hashset
+        
+        P: 
+            1.init a set with the input list
+            2.traverse the hashset
+                2.1) while hashset not has cur-1, curval = cur-1
+                2.2) while hashset has cur+1, curLeng++, cur+1
+                2.3) get the max longest
         '''
         
-        hashset = set(nums)
+        curSet = set(nums)
         longest = 0
         
-        for num in hashset:
-            if num - 1 not in hashset:
+        for num in curSet:
+            if num-1 not in curSet:
                 curVal = num
                 curCnt = 1
                 
-                while curVal + 1 in hashset:
-                    curVal +=1
-                    curCnt +=1
-                    
-                longest = max(longest, curCnt)
+                while curVal+1 in curSet:
+                    curVal+=1
+                    curCnt+=1
                 
+                longest = max(curCnt, longest)
+            
         return longest
