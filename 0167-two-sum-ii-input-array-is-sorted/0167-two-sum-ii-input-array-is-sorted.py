ae7fc472cdb1be76aabb02536e6ea48e8bfc20bd
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-02 13:30:03 -0800

    Time: 90 ms (99.59%), Space: 17.5 MB (67.90%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,25 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        '''
-        U:
-        q) can arr be empty?
-        q) can arr have negative nums?
-        ex) [2,7,11,15] t=9
-            
-        M: two ptrs
-        
-        P: 
-            1. init two ptrs (left and right)
-            2. while left< right:
-                2.1) if arr[left] + arr[right] < target, then left++
-                2.2) elif arr[left] + arr[right] > target, then right--
-                2.3) else return [left+1, right+1]
-        '''
-        
         left, right = 0, len(numbers)-1
         
         while left < right:
             curSum = numbers[left] + numbers[right]
-            if curSum < target: left+=1
-            elif curSum > target: right-=1
-            else: return [left+1, right+1]
+            if curSum > target:
+                right-=1
+            elif curSum < target:
+                left += 1
+            else:
+                return [left+1, right+1]
