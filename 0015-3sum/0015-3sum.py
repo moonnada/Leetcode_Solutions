911f1321c6d880a4bcf7668be617e80f88cd0012
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-05 10:35:10 -0800

    Time: 681 ms (69.98%), Space: 20.8 MB (59.53%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,31 +4,29 @@ class Solution:
         Input: nums = [-1,0,1,2,-1,-4]
         Output: [[-1,-1,2],[-1,0,1]]
         
-        sort = [-4,-1,-1,0,1,2]
-        
-        1. sort the input list
-        2. init 2 ptr (left, right)
         '''
+        
         ans = []
         nums.sort()
         
-        for i,val in enumerate(nums):
-            if i>0 and val == nums[i-1]: continue
+        for i, val in enumerate(nums):
+            if i > 0 and val == nums[i-1]: continue
                 
+            
             left, right = i+1, len(nums)-1
             
             while left < right:
                 threeSum = val + nums[left] + nums[right]
                 
                 if threeSum > 0:
-                    right -=1
+                    right-=1
                 elif threeSum < 0:
                     left += 1
                 else:
                     ans.append([val, nums[left], nums[right]])
-                    left+=1
-                    
+                    left += 1
+                
                     while nums[left] == nums[left-1] and left < right:
                         left+=1
-                        
+                    
         return ans
