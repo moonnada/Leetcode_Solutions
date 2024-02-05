2cc7fb9c973f5e2bcc06c84b5d6bfd1a4a4eb110
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-05 15:04:54 -0800

    Time: 522 ms (79.48%), Space: 29.1 MB (97.85%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,38 +1,35 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         '''
-        U:
-        q) what if input arr is empty?
-         1 2 3 4 5 6 7 8 9
-        [1,8,6,2,5,4,8,3,7]
-         L               R        
+                         0 1 2 3 4 5 6 7 8
+        Input: height = [1,8,6,2,5,4,8,3,7]
+        Output: 49
         
-        M: two ptrs
+        key points: get valid width and height
         
-        P:
-            1. init two ptrs(left and right)
-            2. while left < right: 
-                2.1) get width (right - left)
-                2.2) get height by comparing arr[right] and arr[left] to find min
-                2.3) get area( width * height)
-                2.4) save cur area as max and keep looping to find max
-                2.5) compare elements of left and right and if one of each is less than others then move to the move to next one
+        two pointers
+        
+        1. init two ptrs(left and right)
+        2. while left < right
+            2.1) get a height to compare left and right val(choose the longest one)
+            2.2) get a weight right - left + 1
+            2.3) if arr[left] < arr[right], left++
+            2.4) else right--
+            2.5) get the max container 
         '''
         
         left, right = 0, len(height)-1
-        
-        curArea = maxArea = 0
-        
+        maxContain = 0
         while left < right:
-            width = right - left
             curHeight = min(height[left], height[right])
-            curArea = width * curHeight
-            maxArea = max(curArea, maxArea)
+            curWeight = right - left
             
-            if height[left] <= height[right]:
+            if height[left] < height[right]:
                 left += 1
-            else: right -= 1
+            else:
+                right -= 1
+
                 
-        return maxArea
+            maxContain = max(curHeight*curWeight, maxContain)
             
-            
+        return maxContain
