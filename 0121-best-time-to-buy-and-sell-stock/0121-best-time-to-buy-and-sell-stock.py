71c63e677481cf1ca17f50eebc997d6375f0adf9
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-06 15:04:42 -0800

    Time: 739 ms (78.68%), Space: 27.3 MB (95.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,32 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         '''
-        U:
-        q) can arr be empty?
-        q) is input arr based on integer only?
+        Input: prices = [7,1,5,3,6,4]
+        Output: 5
         
-        ex) [7,1,5,3,6,4]
-             l r
-               l r
-            [7,6,4,3,1]
-             l r
-               l r
-               
-        M: sliding window
-        
-        P:
-            1. init a ptr
-            2. if left >= right, left++
-            3. get the cur profit
-            3. get max profit
         
         '''
-        left = maxProfit = 0
+        left= maxProfit = 0
         
         for right in range(1, len(prices)):
             if prices[left] >= prices[right]: left = right
             else:
                 curProfit = prices[right] - prices[left]
                 maxProfit = max(curProfit, maxProfit)
-                
+            
         return maxProfit
+        
