3a9f15f260c8f5b9d848377197c491415aaaa579
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-07 12:12:05 -0800

    Time: 124 ms (34.09%), Space: 16.6 MB (83.76%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,37 +1,28 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         '''
-        U: 
-        q) can input str or k be empty || 0?
-        Q) input str have only upper letter?
+                       r
+                    l
+        Input: s = "AABABBABB", k = 1
+        Output: 4
         
-        M: hashmap, sliding window
         
-        P:
-            1. init global values( first ptr, map, countFreq, strLen)
-            2. iterate over the input str
-                2.1) count each char and put key and val into map
-                2.2) get max freqNum
-                2.3) init a valid point as boolean. 
-                2.4) if cur point is not valid, remove the left ptr in map, and left ptr is incremented
-                2.5) get the longest len of str
+        while k >0, 
+            r++
+            curLen++
+        
+        
         '''
-       
+        count = {}
+        ans = 0
         left = 0
-        freqMap = {}
-        strLen = 0
-        maxFreq = 0
         
         for right in range(len(s)):
-            freqMap[s[right]] = freqMap.get(s[right], 0) + 1
-            maxFreq = max(maxFreq, freqMap[s[right]])
+            count[s[right]] = count.get(s[right], 0) + 1
             
-            isValid = right - left + 1 - maxFreq <= k
-            
-            if not isValid:
-                freqMap[s[left]] -= 1
+            while right - left +1 - max(count.values()) > k:
+                count[s[left]] -= 1
                 left += 1
                 
-            strLen = right - left + 1
-            
-        return strLen
+            ans = max(ans, right - left +1)
+        return ans
