a77c263816601ac4b4b4414084b81ac5a29e3e71
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-07 09:35:10 -0800

    Time: 742 ms (77.35%), Space: 27.5 MB (55.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,15 +4,17 @@ class Solution:
         Input: prices = [7,1,5,3,6,4]
         Output: 5
         
-        
+        1. init a set and left ptr 
+        2. traverse the input list to find max profit
+            2.1) if cur element > cur left, remove cur element in the set. left ++
+            2.2) else add cur element into the set. calculate the max profit
         '''
-        left= maxProfit = 0
+        
+        left = maxProfit = 0
         
         for right in range(1, len(prices)):
             if prices[left] >= prices[right]: left = right
             else:
-                curProfit = prices[right] - prices[left]
-                maxProfit = max(curProfit, maxProfit)
-            
-        return maxProfit
+                maxProfit = max(maxProfit, prices[right]- prices[left])
         
+        return maxProfit
