36a6394d0922e3143d5df47f3d21b5a57d9d8524
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-07 13:16:11 -0800

    Time: 73 ms (92.69%), Space: 16.7 MB (58.09%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -16,20 +16,38 @@ class Solution:
             2.3)else get the max length
         
         '''
+        #initial solution
+#         count = {}
+#         left = maxLen = 0
         
+#         for right in range(len(s)):
+#             count[s[right]] = count.get(s[right], 0) + 1
+            
+#             while right - left +1 - max(count.values()) > k:
+#                 count[s[left]] -= 1
+#                 left += 1
+                
+            
+#             maxLen = max(maxLen, right - left +1)
+        
+#         return maxLen
+
+        #optimal solution
         count = {}
-        left = maxLen = 0
+        left = maxLen = maxFreq = 0
         
         for right in range(len(s)):
             count[s[right]] = count.get(s[right], 0) + 1
+            maxFreq = max(maxFreq, count[s[right]])
             
-            while right - left +1 - max(count.values()) > k:
+            while right - left + 1 - maxFreq > k:
                 count[s[left]] -= 1
                 left += 1
                 
-            
             maxLen = max(maxLen, right - left +1)
-        
         return maxLen
+
+
+
             
        
