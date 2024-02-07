6bed7fd1ad4fce1508557ea60b8d8673a66bd1b8
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-07 14:45:13 -0800

    Time: 52 ms (89.16%), Space: 16.7 MB (64.49%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,42 +1,29 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         '''
-        U:
-            permutation only?
-            q) s1 len is alway smaller than s2?
-            q) s1 and s2 consist only lowercases?
-            
-            ex) s1= 'ab', s2 = 'eidbaooo'
-                newS1 = ba
-            
-        M: substring
+                                ab
+        Input: s1 = "ab", s2 = "eidbaooo"
+        Output: true
         
-        P:
-            1. check edge cases(empty, s1 and s2 length)
-            2. init a global values of count freq of both strs
-            3. traverse the input str1 to get freq
-            4. if both freq are equal, then reture true\
-            5. traverse the input str2 to slide a window
-                5.1) sliding window is incremented
-                5.2) remove the left side of window
-                5.3) if freq of both strs are equal, then return
-            6. return false
+     
+            
+        
         '''
-        if len(s1) > len(s2): return False
-        
+        if len(s2) < len(s1):
+            return False
         freqS1 = [0] * 26
         freqS2 = [0] * 26
         
         for i in range(len(s1)):
-            freqS1[ord(s1[i]) - 97] += 1
-            freqS2[ord(s2[i]) - 97] += 1
-            
+            freqS1[ord(s1[i]) - ord('a')] += 1
+            freqS2[ord(s2[i]) - ord('a')] += 1
+        
         if freqS1 == freqS2: return True
         
         for i in range(len(s1), len(s2)):
-            freqS2[ord(s2[i]) - 97] += 1
-            freqS2[ord(s2[i - len(s1)]) - 97] -= 1
+            freqS2[ord(s2[i]) - ord('a')] += 1
+            freqS2[ord(s2[i - len(s1)]) - ord('a')] -= 1
             
             if freqS1 == freqS2: return True
-            
+        
         return False
