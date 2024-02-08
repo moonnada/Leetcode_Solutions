14bba78a67f4e9fa11f66b0fcf23e13386f6d2e0
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-08 14:30:02 -0800

    Time: 37 ms (56.52%), Space: 16.7 MB (59.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        '''
-        1. loop the input s and if one of closed parentheses is found, pop its open bracket
-        2. else // open bracket, push it to the stack
-        '''
         
         stack = []
         pairs = {
-            '(': ')',
-            '{': '}',
-            '[':']'
+            '(' : ')',
+            '{' : '}',
+            '[' : ']'
         }
         
         for i in s:
             if i in pairs:
                 stack.append(i)
+            elif not stack or pairs[stack[-1]] != i:
+                return False
             else:
-                if not stack or pairs[stack[-1]] != i:
-                    return False
-                else:
-                    stack.pop()
-            
-        return len(stack) == 0
+                stack.pop()
+        
+        return not stack 
+                
+        
+        
