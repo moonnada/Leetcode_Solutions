dfdc2102cedc7af95648aba30fe2f45645307b9b
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-08 10:58:39 -0800

    Time: 126 ms (32.13%), Space: 16.7 MB (69.80%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,53 +1,23 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         '''
-                    0123
-                       r
-                    l
-        Input: s = "AABABBABB", k = 1
-        Output: 4
-        A:4
-        B:5
-        
-        1. init a map to count each alphabet in the str
-        2. iterative over the input str to count each alphabet in the map
-            2.1)count each letter
-            2.2)if cur position - max countVal > k, left++, remove the curleft in map
-            2.3)else get the max length
-        
+        1. init a hashmap
+        2. count each val in the input str
+        3. iterative the input str to get the longest 
+            3.1) if right - left +1 - max hashmap val > k, then remove the cur val in the map, left++
+            3.2) else get the length
         '''
-        #initial solution
-#         count = {}
-#         left = maxLen = 0
         
-#         for right in range(len(s)):
-#             count[s[right]] = count.get(s[right], 0) + 1
-            
-#             while right - left +1 - max(count.values()) > k:
-#                 count[s[left]] -= 1
-#                 left += 1
-                
-            
-#             maxLen = max(maxLen, right - left +1)
-        
-#         return maxLen
-
-        #optimal solution
         count = {}
-        left = maxLen = maxFreq = 0
+        left = longest = 0
         
         for right in range(len(s)):
             count[s[right]] = count.get(s[right], 0) + 1
-            maxFreq = max(maxFreq, count[s[right]])
             
-            while right - left + 1 - maxFreq > k:
+            while right - left + 1 - max(count.values()) > k:
                 count[s[left]] -= 1
                 left += 1
                 
-            maxLen = max(maxLen, right - left +1)
-        return maxLen
-
-
-
+            longest = max(longest, right-left+1)
             
-       
+        return longest
