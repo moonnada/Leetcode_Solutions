bad6c9c91f6468591c65bc31239ad8671f2d35e8
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-08 12:02:10 -0800

    Time: 51 ms (91.02%), Space: 16.9 MB (53.51%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,25 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        '''
-        Input: s1 = "ab", s2 = "eidbaooo"
-        Output: true
-        
-        
-        '''
         if len(s1) > len(s2): return False
-        
-        freqS1, freqS2 = [0]*26, [0]*26
+        countS1, countS2 = [0] * 26, [0]*26
+        left = 0
         
         for i in range(len(s1)):
-            freqS1[ord(s1[i]) - ord('a')] += 1
-            freqS2[ord(s2[i]) - ord('a')] += 1
+            countS1[ord(s1[i]) - ord('a')] += 1
+            countS2[ord(s2[i]) - ord('a') ] += 1
             
-        if freqS1 == freqS2: return True
+        if countS1 == countS2: return True
         
-        for i in range(len(s1), len(s2)):
-            freqS2[ord(s2[i]) - ord('a')] += 1
-            freqS2[ord(s2[i - len(s1)]) - ord('a')] -= 1
+        for r in range(len(s1), len(s2)):
+            countS2[ord(s2[r]) - ord('a')] += 1
+            countS2[ord(s2[r - len(s1)]) - ord('a')] -= 1
             
-            if freqS1 == freqS2: return True
+            if countS1 == countS2: return True
             
         return False
+         
