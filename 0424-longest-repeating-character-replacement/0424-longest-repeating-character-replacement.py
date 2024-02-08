6c9ccdae3d344f8cb0e6c02a08df5af5ed38173d
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-08 11:53:35 -0800

    Time: 131 ms (28.12%), Space: 16.7 MB (70.21%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,24 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        '''
-        1. init a hashmap
-        2. count each val in the input str
-        3. iterative the input str to get the longest 
-            3.1) if right - left +1 - max hashmap val > k, then remove the cur val in the map, left++
-            3.2) else get the length
-        '''
-        
         count = {}
-        left = longest = 0
-        maxFreq = 0
+        longest = left = 0
+        
         for right in range(len(s)):
             count[s[right]] = count.get(s[right], 0) + 1
-            maxFreq = max(maxFreq, count[s[right]])
             
-            while right - left + 1 - maxFreq> k:
+            while right-left +1 - max(count.values()) > k:
                 count[s[left]] -= 1
                 left += 1
                 
-            longest = max(longest, right-left+1)
-            
+            longest = max(longest, right - left + 1)
+        
         return longest
+        
