08193c4996ec92e2ce550ce5e34a79bfcefa9a1d
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-08 08:11:55 -0800

    Time: 42 ms (99.11%), Space: 16.7 MB (64.49%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,27 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         '''
-                                ab
         Input: s1 = "ab", s2 = "eidbaooo"
         Output: true
         
-     
-            
         
         '''
         if len(s1) > len(s2): return False
         
-        freqS1, freqS2 = [0] * 26, [0]*26
+        freqS1, freqS2 = [0]*26, [0]*26
         
         for i in range(len(s1)):
-            freqS1[ord(s1[i]) - ord('a') ] += 1
+            freqS1[ord(s1[i]) - ord('a')] += 1
             freqS2[ord(s2[i]) - ord('a')] += 1
             
         if freqS1 == freqS2: return True
         
         for i in range(len(s1), len(s2)):
             freqS2[ord(s2[i]) - ord('a')] += 1
-            freqS2[ord(s2[i- len(s1)]) - ord('a')] -= 1
+            freqS2[ord(s2[i - len(s1)]) - ord('a')] -= 1
             
             if freqS1 == freqS2: return True
             
