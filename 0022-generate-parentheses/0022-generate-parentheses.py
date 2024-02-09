18aeacee336fe1bbcbf3d74e0eb0b1d79d53ca13
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-09 12:02:55 -0800

    Time: 36 ms (75.22%), Space: 16.9 MB (67.83%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,13 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
+        '''
+        Input: n = 3
+        Output: ["((()))","(()())","(())()","()(())","()()()"]
+        
+        only add open brackets if open < n
+        only add a closing bracked if closed < open
+        valid if open == closed == n
+        
+        '''
         stack = []
+        ans = []
         
         def backtrack(openN, closedN):
             if openN == closedN == n:
-                res.append("".join(stack))
+                ans.append("".join(stack))
                 return
-            
             if openN < n:
                 stack.append("(")
                 backtrack(openN + 1, closedN)
@@ -19,5 +27,4 @@ class Solution:
                 stack.pop()
                 
         backtrack(0,0)
-        
-        return res
+        return ans
