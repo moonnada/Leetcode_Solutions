680b066355b2dcb8e4dd78c931eb1cfa6175e541
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-09 07:45:07 -0800

    Time: 41 ms (31.70%), Space: 16.6 MB (59.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,22 +1,29 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        '''
+        stack
+        
+        1. init a stack
+        2. init a dic for valid parentheses
+        3. iterative the input str to find valid parenthese
+            3.1) if cur char is an open bracket, append it to the stack
+            3.2) else if stack is empty or cur char is not matched with the recent stack val, return False
+            3.3) else pop from stack
+        4. return true if stack is empty
+        '''
         
         stack = []
-        pairs = {
-            '(' : ')',
-            '{' : '}',
-            '[' : ']'
+        valid = { 
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
         }
         
         for i in s:
-            if i in pairs:
+            if i in valid:
                 stack.append(i)
-            elif not stack or pairs[stack[-1]] != i:
+            elif not stack or valid[stack[-1]] != i: 
                 return False
             else:
                 stack.pop()
-        
-        return not stack 
-                
-        
-        
+        return True if not stack else False
