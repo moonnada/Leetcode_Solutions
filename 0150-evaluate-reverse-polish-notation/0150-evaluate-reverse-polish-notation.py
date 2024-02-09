f264aab867cf01ffe81c92b049a7a1e4a70483f1
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-09 15:49:36 -0800

    Time: 66 ms (61.22%), Space: 17.1 MB (81.62%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,52 +1,39 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         '''
-        U:
-            q) do I return always integer when I divide each num
-            q) can input list have operators first?
-            
-            ex) [2,1,+,3,5,-]
-            
-            s = [2,1]
-            
-            
-            
-        M: stack
-        
-        P: 
-            1. init a stack
-            2. traverse the input list to put val into stack
-                2.1) if curVal is num, put into stack
-                2.2) if not, calculate the second top of stack and top of stack. once calculating is done, put the new val into stack\
-            3. return ans
+        Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+        Output: 22
         
         
+        
+        if cur char is num, put into stack
+        else // cur char is an operator
+            val2 = stack.pop
+            val1 = stack.pop
+            
+            newVal = val1, cur operater, val2
+            stack.append(newVal)
         '''
-        ans = 0
+        
         stack = []
-        
-        
+        val1 = val2 = ""
         for i in tokens:
             if i not in "+-*/":
                 stack.append(int(i))
                 continue
-            
-            num2 = stack.pop()
-            num1 = stack.pop()
+                
+            val2 = stack.pop()
+            val1 = stack.pop()    
             
             if i == "+":
-                ans = num1 + num2
-            
-            elif i == "*":
-                ans = num1 * num2
-                
+                newVal = val1 + val2
             elif i == "-":
-                ans = num1 - num2
-                
+                newVal = val1 - val2
+            elif i == "*":
+                newVal = val1 * val2
             elif i == "/":
-                ans = int(num1 / num2)
+                newVal = int(val1 / val2)
                 
-            stack.append(ans)
-            
+            stack.append(newVal)
+                
         return stack.pop()
-        
