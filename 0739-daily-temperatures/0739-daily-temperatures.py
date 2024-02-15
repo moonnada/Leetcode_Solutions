3901b13262cc21c1851a2de492f7a8fa7bc47354
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-15 14:35:45 -0800

    Time: 895 ms (62.10%), Space: 33 MB (11.06%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,36 +1,23 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         '''
-        U: 
-            key points: can be below 30, no future day then put 0 
-            
-            ex) t = [73, 74, 75, 71, 69. 72, 76, 73]
-              ans = [1,1,4,2,1,1,0,0]
-              
-        M: stack
+        Input: temperatures = [73,74,75,71,69,72,76,73]
+        Output: [1,1,4,2,1,1,0,0]
         
-        P: 
-            1. init a res list. put 0 as much as len of input list
-            2. init a stack
-            3. traverse the input list to put index and value of list
-                3.1) while stack and curVal > stack's top
-                    3.2) pop temp and index from stack
-                    3.3) put curIndex - stackIndex into ans list. 
-                4) put cur temp and index into stack
-            5. return list
-            
+        1. init a stack and an ans list
+        2. iterative the input list from 1 to end 
+           
         
-        t: o(n), s: o(n)
         '''
         
         res = [0] * len(temperatures)
-        stack = [] 
+        stack = []
         
         for i,t in enumerate(temperatures):
             while stack and t > stack[-1][0]:
-                stackT, stackInd = stack.pop()
-                res[stackInd] = i - stackInd
+                stackT, stackIdx = stack.pop()
+                res[stackIdx] = i - stackIdx
             
-            stack.append([t, i])
+            stack.append([t,i])
             
         return res
