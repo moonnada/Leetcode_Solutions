4e7e1cc0842e2fdf11fd86652b866cf2b902d0ad
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-21 12:55:39 -0800

    Time: 54 ms (78.18%), Space: 51.2 MB (38.88%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -11,40 +11,16 @@
  * @return {number[]}
  */
 var rightSideView = function(root) {
-    /*
-    1. check empty root
-    2. init a queue and ans
-    3. while queue exist
-        3.1) shift curNode and curheight from queue
-        3.2) if curHeight === ans.length, then put the cur value into ans 
-        3.3) if curNode.right exists, put into queue and height++
-        3.4) if curNode.left exists, put into queue and height++
-    */
-    
-//     if(!root) return [];
-//     let queue = [[root, 0]];
-//     let ans = [];
-    
-//     while(queue.length){
-//         let [curNode, level] = queue.shift();
-        
-//         if(ans.length === level) ans.push(curNode.val);
-        
-//         curNode.right && queue.push([curNode.right, level+1]);
-//         curNode.left && queue.push([curNode.left, level+1]);
-//     }
-//     return ans
-    
     if(!root) return [];
+    let queue = [[root, 0]];
     let ans = [];
-    pre(root, 0);
-    return ans;
     
-    function pre(node, h){
-        if(!node) return;
-        ans[h] = node.val;
-        pre(node.left, h+1)
-        pre(node.right, h+1)
+    while(queue.length){
+        let [curNode, level] = queue.shift();
+        if(ans.length === level) ans.push(curNode.val);
         
+        curNode.right && queue.push([curNode.right, level+1])
+        curNode.left && queue.push([curNode.left, level+1])
     }
+    return ans
 };
