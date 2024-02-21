a154da95dda7795cc8782913440792ea90c44c3c
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-21 09:17:25 -0800

    Time: 71 ms (65.61%), Space: 59.1 MB (31.00%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,15 +13,9 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    // while(root){
-    //     if(root.val > p.val && root.val > q.val){
-    //         root = root.left
-    //     } else if(root.val < p.val && root.val < q.val){
-    //         root = root.right
-    //     } else break;
-    // } return root
-    
-    if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p,q);
-    else if(root.val < p.val && root.val < q.val ) return lowestCommonAncestor(root.right, p,q);
+    if(root.val > p.val && root.val >q.val) return lowestCommonAncestor(root.left, p,q)
+    else if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
     else return root
+    
+    
 };
