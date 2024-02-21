99616515520cd6cebb21a9775700786a92267f04
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-21 12:57:36 -0800

    Time: 55 ms (74.56%), Space: 51.5 MB (18.67%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -11,16 +11,31 @@
  * @return {number[]}
  */
 var rightSideView = function(root) {
-    if(!root) return [];
-    let queue = [[root, 0]];
-    let ans = [];
+    //bfs
+//     if(!root) return [];
+//     let queue = [[root, 0]];
+//     let ans = [];
     
-    while(queue.length){
-        let [curNode, level] = queue.shift();
-        if(ans.length === level) ans.push(curNode.val);
+//     while(queue.length){
+//         let [curNode, level] = queue.shift();
+//         if(ans.length === level) ans.push(curNode.val);
         
-        curNode.right && queue.push([curNode.right, level+1])
-        curNode.left && queue.push([curNode.left, level+1])
+//         curNode.right && queue.push([curNode.right, level+1])
+//         curNode.left && queue.push([curNode.left, level+1])
+//     }
+//     return ans
+    
+    //dfs
+    if(!root) return [];
+    let ans = [];
+    pre(root, 0);
+    return ans;
+    
+    function pre(node, level){
+        if(!node) return;
+        
+        ans[level] = node.val;
+        pre(node.left, level+1)
+        pre(node.right, level+1)
     }
-    return ans
 };
