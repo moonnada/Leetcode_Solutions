42de99148298bf64029039744a73d948444a8676
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-21 15:07:30 -0800

    Time: 60 ms (50.34%), Space: 51.4 MB (28.48%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -12,21 +12,30 @@
  */
 var rightSideView = function(root) {
     //bfs
+    /*
+    1.init a queue [node, level]
+    2. check edge case(empty)
+    3. init an ans arr
+    4. while queue exists,
+        4.1) check the cur node and its level from queue
+        4.2) if ans.length = cur level, then put the cur val into the ans arr
+        4.3) if curval has the right child, then put the val into queue
+        4.4) if curval has the left child, put the val into que
+    5. return ans
+    */
     if(!root) return [];
     let queue = [[root, 0]];
     let ans = [];
     
     while(queue.length){
-        let [curVal, curLevel] = queue.shift();
+        let [curNode, level] = queue.shift();
+        if(ans.length === level) ans.push(curNode.val)
         
-        if(ans.length === curLevel){
-            ans.push(curVal.val)
-        }
-        
-        curVal.right && queue.push([curVal.right, curLevel+1])
-        curVal.left && queue.push([curVal.left, curLevel+1])
+        if(curNode.right) queue.push([curNode.right, level+1])
+        if(curNode.left) queue.push([curNode.left, level+1])
     }
     return ans
+    
     //dfs
 //     if(!root) return [];
 //     let ans = [];
