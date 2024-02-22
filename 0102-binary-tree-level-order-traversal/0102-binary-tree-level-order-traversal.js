a56f272191248d1ef7c509622e748f64b5491303
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-22 07:53:40 -0800

    Time: 85 ms (21.24%), Space: 54.4 MB (46.10%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -12,29 +12,22 @@
  */
 var levelOrder = function(root) {
     //bfs
-    /*
-    1. check edge cases(empty, one node)
-    2. init a queue with the root noode
-    3. while queue exists
-        3.1) pop the cur val
-        3.2) if cur val's left exist, put the val into queue
-        3.3) else if cur val's right exists, put th
-    */
     if(!root) return [];
     let queue = [root];
     let ans = [];
     
     while(queue.length){
         let curLevel = [];
-        let queLevel = queue.length
+        let queLevel = queue.length;
         for(let i=0; i<queLevel; i++){
-            let curVal = queue.shift();
-            if(curVal.left) queue.push(curVal.left);
-            if(curVal.right) queue.push(curVal.right);
+            let curNode = queue.shift();
+            if(curNode.left) queue.push(curNode.left);
+            if(curNode.right) queue.push(curNode.right);
             
-            curLevel.push(curVal.val)
+            curLevel.push(curNode.val)
         }
         ans.push(curLevel)
     }
     return ans
+    
 };
