692ad7779d3621aa576755e5263ac37018ad9986
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-21 16:48:06 -0800

    Time: 72 ms (20.26%), Space: 56.1 MB (5.03%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -12,19 +12,27 @@
  */
 var isValidBST = function(root) {
     /*
-    dfs using recursion
-        1. in a helper func, check bst or not
+    1. check edge cases(empty)
+    2. init a queue
+    3. while queue exists
+        3.1) get the cur val from queue
+        3.2) if the cur val has left child, check less than the curval. if isnt return false, else put into que
+        3.3)elif the cur val has right child, check greater than the curval. if isnt return false, else put into que
+    4. return true  
+    
     */
+    if(!root) return false
+    let queue = [[root, -Infinity, Infinity]];
     
-    if(!root) return false;
-    if(root.length === 1) return true;
-    
-    return checkValid(root, -Infinity, Infinity);
-    
-    function checkValid(node, lower, upper){
-        if(!node) return true;
-        if((lower < node.val) && (upper > node.val)){
-            return checkValid(node.left, lower, node.val) && checkValid(node.right, node.val, upper)
-        } else return false
+    while(queue.length){
+        let [curNode, minVal, maxVal] = queue.shift();
+        
+        if(!curNode) continue;
+        if(curNode.val >= maxVal || curNode.val <= minVal) return false
+        
+        queue.push([curNode.left, minVal, curNode.val])
+        queue.push([curNode.right, curNode.val, maxVal])
     }
+    return true
+   
 };
