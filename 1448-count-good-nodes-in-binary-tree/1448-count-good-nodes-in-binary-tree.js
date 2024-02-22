b49a84f7b3a71c56edb5f3330344cf5b13b41449
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-22 11:40:04 -0800

    Time: 123 ms (56.87%), Space: 70.1 MB (51.34%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -11,20 +11,15 @@
  * @return {number}
  */
 var goodNodes = function(root) {
-    //dfs
     let cnt = 0;
-    // helper(root.left, root.val);
-    // helper(root.right, root.val);
-    helper(root, root.val)
+    helper(root, root.val);
     return cnt;
     
     function helper(node, maxVal){
         if(!node) return;
-        
         if(node.val >= maxVal) cnt++;
         
         helper(node.left, Math.max(node.val, maxVal));
         helper(node.right, Math.max(node.val, maxVal))
     }
-    
 };
