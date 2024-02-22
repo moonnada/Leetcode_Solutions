08f7ddfd4fd747ef5b24bbc52b9cd991e4f4998d
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-22 12:00:24 -0800

    Time: 59 ms (75.43%), Space: 53.7 MB (15.42%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -12,40 +12,18 @@
  */
 var isValidBST = function(root) {
     /*
-    1. check edge cases(empty)
-    2. init a queue
-    3. while queue exists
-        3.1) get the cur val from queue
-        3.2) if the cur val has left child, check less than the curval. if isnt return false, else put into que
-        3.3)elif the cur val has right child, check greater than the curval. if isnt return false, else put into que
-    4. return true  
-    
+    Always left child < root.val < right child
     */
-    //bfs
-//     if(!root) return false
-//     let queue = [[root, -Infinity, Infinity]];
-    
-//     while(queue.length){
-//         let [curNode, minVal, maxVal] = queue.shift();
-        
-//         if(!curNode) continue;
-//         if(curNode.val >= maxVal || curNode.val <= minVal) return false
-        
-//         queue.push([curNode.left, minVal, curNode.val])
-//         queue.push([curNode.right, curNode.val, maxVal])
-//     }
-//     return true
-   
     if(!root) return false;
     if(root.length === 1) return true;
     
-    return helper(root, -Infinity, Infinity)
+    return helper(root, -Infinity, Infinity);
     
-    function helper(node, lower, upper){
+    function helper(node, minVal, maxVal){
         if(!node) return true;
-        if((lower < node.val) && (node.val < upper)){
-            return helper(node.left, lower, node.val) && helper(node.right, node.val, upper)
+        
+        if((minVal < node.val) && (node.val < maxVal)){
+            return helper(node.left,  minVal, node.val) && helper(node.right, node.val, maxVal)
         } return false
     }
-    
 };
