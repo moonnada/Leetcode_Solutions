17b18cbdd3d51ea6c0543ec952af2554cb09ff6e
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-21 16:57:11 -0800

    Time: 54 ms (90.85%), Space: 53.6 MB (20.92%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -21,18 +21,31 @@ var isValidBST = function(root) {
     4. return true  
     
     */
-    if(!root) return false
-    let queue = [[root, -Infinity, Infinity]];
+    //bfs
+//     if(!root) return false
+//     let queue = [[root, -Infinity, Infinity]];
     
-    while(queue.length){
-        let [curNode, minVal, maxVal] = queue.shift();
+//     while(queue.length){
+//         let [curNode, minVal, maxVal] = queue.shift();
         
-        if(!curNode) continue;
-        if(curNode.val >= maxVal || curNode.val <= minVal) return false
+//         if(!curNode) continue;
+//         if(curNode.val >= maxVal || curNode.val <= minVal) return false
         
-        queue.push([curNode.left, minVal, curNode.val])
-        queue.push([curNode.right, curNode.val, maxVal])
-    }
-    return true
+//         queue.push([curNode.left, minVal, curNode.val])
+//         queue.push([curNode.right, curNode.val, maxVal])
+//     }
+//     return true
    
+    if(!root) return false;
+    if(root.length === 1) return true;
+    
+    return helper(root, -Infinity, Infinity)
+    
+    function helper(node, lower, upper){
+        if(!node) return true;
+        if((lower < node.val) && (node.val < upper)){
+            return helper(node.left, lower, node.val) && helper(node.right, node.val, upper)
+        } return false
+    }
+    
 };
