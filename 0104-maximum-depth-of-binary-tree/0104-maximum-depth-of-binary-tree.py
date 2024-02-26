148ec05152e347591d4e49ed5abcd0f83c90d2a3
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-26 15:51:27 -0800

    Time: 35 ms (88.26%), Space: 17.7 MB (42.90%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,38 +6,14 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        '''
-        U:
-            ex) root can be empty?
-            ex) the input tree is considered as a bst?
-            
-        M: BST
-        
-        P: 
-            1. check edge case (null)
-            2. init a que and detph as 0
-            3. while que
-                3.0) loop a cur level of children
-                3.1) curNode is popped from que
-                3.2) if curnode.left, add the node into queue
-                3.3) if curnode.right, add the node into queue
-                3.4) depth++
-            4. return depth
-        '''
-        
         if not root: return 0
-        depth = 0
-        que = deque([root])
         
-        while que:
-            for _ in range(len(que)):
-                curNode = que.popleft()
-                if curNode.left:
-                    que.append(curNode.left)
-                    
-                if curNode.right:
-                    que.append(curNode.right)
-            
-            depth += 1
-            
-        return depth
+        if not root.right:
+            return 1 + self.maxDepth(root.left)
+        
+        if not root.left:
+            return 1 + self.maxDepth(root.right)
+        
+        else:
+            return 1+ max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
