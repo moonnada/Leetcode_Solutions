5ef000deb5ebcaf42079d82bfa8c41b9d6399678
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-26 14:54:51 -0800

    Time: 38 ms (75.94%), Space: 17.2 MB (71.00%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,52 +6,27 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        '''
-        U:
-            q) node val is only integer?
-            
-        M: BFS
+        if not root:
+            return []
         
-        P:
-            1. check edge case(null)
-            2. init an ans list and queue
-            3. while que exists
-                3.1) pop cur node from queue
-                3.2) visit cur level of node by using a loop    
-                    3.3) put cur node into queue and list
-            4. return list        
-            
-            
-        M: DFS
+        queue = [root]
+        ans = []
         
-        P:
-            1. check edge case(null)
-            2. init an ans list and level value
-            3. init a helper fuc
-                3.1) if len of list == level, start a new []
-                3.2) put cur node val into list
-                3.3) if curnode.left exists, visit there
-                3.4) if curnode.right exists, visit there
-        '''
-
-        if not root: return []
-        
-        levels = []
-        
-        def helper(node, level):
-            if len(levels) == level: 
-                 levels.append([])
-
-            levels[level].append(node.val)
+        while queue:
+            curLevel = []
+            curQue = len(queue)
             
-            if node.left:
-                helper(node.left, level+1)
+            for i in range(curQue):
+                curNode = queue.pop(0)
                 
-            if node.right:
-                helper(node.right, level+1)
-                
-        helper(root, 0)
-        
-        return levels
-        
-        
+                if curNode.left:
+                    queue.append(curNode.left)
+                    
+                if curNode.right:
+                    queue.append(curNode.right)
+                    
+                curLevel.append(curNode.val)
+            
+            ans.append(curLevel)
+
+        return ans
