e05e41cfa7275980deb9c77a913e9de962d947d5
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-27 15:41:50 -0800

    Time: 42 ms (61.63%), Space: 17.7 MB (73.92%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -16,7 +16,7 @@ class Solution:
             4.1) if not node, return 0
             4.2) if node.left exists, visit there level + 1
             4.3) right, level +1
-        '''
+        
         if not root: return 0
         
         maxDia = 0
@@ -43,22 +43,21 @@ class Solution:
             maxDia = max(leftHeight+ rightHeight, maxDia)
         
         return maxDia
+        '''
+        if not root: return 0
+        self.diameter = 0
+        
+        def helper(node):
+            if not node: return 0
+            left = helper(node.left)
+            right = helper(node.right)
+            self.diameter = max(self.diameter, left+right)
+            return max(left, right)+1
+        
+        helper(root)
+        return self.diameter
         
         
-#         if not root: return 0
-#         diameter = 0
-        
-#         def helper(node, level):
-#             if not node: return 
-            
-#             if node.left:
-#                 helper(node.left, level+1)
-                
-#             if node.right:
-#                 helper(node.right, level+1)
-                
-#             return max(helper(node.left, level+1), helper(node.right, level+1))
-        
-#         return helper(root, diameter)
+
 
     
