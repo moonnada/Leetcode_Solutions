e937dbbfa6e83625af92d804ec290d2f30507769
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-28 11:15:57 -0800

    Time: 76 ms (61.23%), Space: 16.6 MB (95.32%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,19 +6,46 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root: return False
+        if not root and not subRoot:
+            return True
         
-        def sameTree(root, subRoot):
-            if not root and not subRoot: 
+        def isSameTree(root, subRoot):
+            if not root and not subRoot:
                 return True
             
-            if root and subRoot:
-                return root.val == subRoot.val and sameTree(root.left, subRoot.left) and sameTree(root.right, subRoot.right)
-            
-        if sameTree(root, subRoot): return True
-    
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-    
+            if root and subRoot and root.val == subRoot.val:
+                return isSameTree(root.left, subRoot.left) and isSameTree(root.right, subRoot.right)
+            else:
+                return False
         
+        if root and subRoot:
+            queue = [root]
+            while queue:
+                for i in range(len(queue)):
+                    curNode = queue.pop(0)
+                    
+                    if curNode.left:
+                        queue.append(curNode.left)
+                    if curNode.right:
+                        queue.append(curNode.right)
+                    if curNode.val == subRoot.val:
+                        if isSameTree(curNode, subRoot):
+                            return True
+        return False
             
+#         if not root: 
+#             return False
+        
+#         def isSameTree(root, subRoot):
+#             if not root and not subRoot:
+#                 return True
+            
+#             if root and subRoot:
+#                 return root.val == subRoot.val and isSameTree(root.left, subRoot.left) and isSameTree(root.right, subRoot.right)
+            
+#         if isSameTree(root, subRoot): return True 
+        
+#         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+        
         
