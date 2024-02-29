df8293450452d8b2d0f81bf846540eec05a756e0
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-28 17:50:55 -0800

    Time: 38 ms (39.43%), Space: 16.6 MB (66.91%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,48 +6,19 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        '''
-        U:
-            q) input tree can be null? 
-        '''
+        if not p and not q:
+            return True
         
-        '''
-            #DFS
-            1. check edge cases(null, one node)
-            2. check cur values
-            3. recursion both sides
-        '''
-#         if not p and not q: return True
+        if not p or not q:
+            return False
         
-#         if not p or not q: return False
-        
-#         if p.val != q.val: return False
-        
-#         return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
-
-        '''
-            #BFS
-            1. check edge cases(null, one node)
-            2. init a queue for both trees
-            3. while queue exists
-                3.1) pop a val from each queue
-                3.2) compare the values. if there are same, put each side's val into queue
-                3.3) if p and q are different nodes, then false
-            4. true
-        '''
-        
-        if not p and not q: return True
-        if not p or not q: return False
-        if p.val != q.val: return False
-    
-        que = deque([(p,q)])
-        while que:
-            p,q = que.popleft()
-            if p and q and p.val == q.val:
-                que.append((p.left, q.left))
-                que.append((p.right, q.right))
-            
-            elif p is not q:
+        def helper(p, q):
+            if not p and not q: 
+                return True
+            if not p or not q:
                 return False
+            
+            return p.val == q.val and helper(p.left, q.left) and helper(p.right, q.right)
         
-        return True
+        return p.val == q.val and helper(p.left, q.left) and helper(p.right, q.right)
+            
