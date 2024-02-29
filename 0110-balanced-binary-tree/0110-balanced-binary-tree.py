9489c0128331632d22bc4db02557e3936213036f
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-29 15:27:02 -0800

    Time: 47 ms (52.60%), Space: 17.7 MB (69.00%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,54 +6,16 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        '''
+        diff height > 1, false
+        '''
         
         if not root: return True
         
-        queue = [root]
-        leftHeight, rightHeight = 0,0
-        
         def getHeight(node):
-            if not node:
-                return 0
+            if not node: return 0
+            
             return 1 + max(getHeight(node.left), getHeight(node.right))
         
-        while queue:
-            for i in range(len(queue)):
-                curNode = queue.pop(0)
-                leftHeight = getHeight(curNode.left)
-                rightHeight = getHeight(curNode.right)
-                
-                if abs(leftHeight - rightHeight) > 1:
-                    return False
-                
-                if curNode.left:
-                    queue.append(curNode.left)
-                    
-                if curNode.right:
-                    queue.append(curNode.right)
-                    
-        return True
-                
-                
-        '''
-        unbalanced ? height difference > 1
-        dfs
-        
-        1. check edge case
-        2. init a height as 0
-        3. make a helper func to track height
-            3.1) in a helper func, first check if node not exist, return
-            3.2) left = node.left , right = node.right
-            3.3) if abs(left - right) > 1: return false, else true
+        return abs(getHeight(root.left) - getHeight(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
             
-            
-        
-        '''
-#         if not root: return True
-        
-#         def getHeight(node):
-#             if not node: return -1
-            
-#             return 1 + max(getHeight(node.left) , getHeight(node.right))
-        
-#         return abs(getHeight(root.left) - getHeight(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
