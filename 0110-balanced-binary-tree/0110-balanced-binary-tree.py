792694d1edc50ba3d81f3b8fd79d06de6c22eb21
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-28 22:12:44 -0800

    Time: 69 ms (6.35%), Space: 17.7 MB (45.98%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -7,16 +7,24 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         '''
-        U:
-            key point
+        unbalanced ? height difference > 1
+        dfs
+        
+        1. check edge case
+        2. init a height as 0
+        3. make a helper func to track height
+            3.1) in a helper func, first check if node not exist, return
+            3.2) left = node.left , right = node.right
+            3.3) if abs(left - right) > 1: return false, else true
+            
+            
         
         '''
         if not root: return True
         
-        return abs(self.height(root.left) - self.height(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
-    
-    
-    def height(self, root):
-        if not root: return -1
+        def getHeight(node):
+            if not node: return -1
+            
+            return 1 + max(getHeight(node.left) , getHeight(node.right))
         
-        return 1 + max(self.height(root.left), self.height(root.right))
+        return abs(getHeight(root.left) - getHeight(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
