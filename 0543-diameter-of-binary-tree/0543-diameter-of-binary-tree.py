fb8aba08ab7affaf7763c016ccaefcd75fcc757f
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-02-29 13:07:55 -0800

    Time: 42 ms (59.63%), Space: 17.6 MB (68.06%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,58 +6,18 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        '''
-        dfs
-        
-        1. check edge case
-        2. init a diameter val 
-        3. init a helper func (node, level)
-        4. in a helper func,   
-            4.1) if not node, return 0
-            4.2) if node.left exists, visit there level + 1
-            4.3) right, level +1
-        
-        if not root: return 0
-        
-        maxDia = 0
-        queue = [root]
-        
-        def getHeight(node):
-            if not node: return 0
-            
-            return 1 + max(getHeight(node.left), getHeight(node.right))
-        
-        while queue:
-            curNode = queue.pop(0)
-            
-            leftHeight = getHeight(curNode.left)
-            rightHeight = getHeight(curNode.right)
-
-
-            if curNode.left:
-                queue.append(curNode.left)
-                
-            if curNode.right:
-                queue.append(curNode.right)
-            
-            maxDia = max(leftHeight+ rightHeight, maxDia)
-        
-        return maxDia
-        '''
         if not root: return 0
         self.diameter = 0
         
         def helper(node):
             if not node: return 0
+                
             left = helper(node.left)
             right = helper(node.right)
-            self.diameter = max(self.diameter, left+right)
-            return max(left, right)+1
+            
+            self.diameter = max(self.diameter, left + right)
+            return max(left, right) + 1
         
         helper(root)
+        
         return self.diameter
-        
-        
-
-
-    
