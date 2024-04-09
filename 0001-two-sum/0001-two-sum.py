6f302e98c555cd1adea853d7e4129239b33305df
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-09 11:51:33 -0700

    Time: 45 ms (98.81%), Space: 17.8 MB (16.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        newMap = defaultdict(int)
+        newMap = {}
         
         for i in range(len(nums)):
             lookFor = target - nums[i]
@@ -10,11 +9,3 @@ class Solution:
             else:
                 newMap[nums[i]] = i
         
-        # newMap = {}
-        # for i in range(len(nums)):
-        #     lookFor = target - nums[i]
-        #     if lookFor in newMap:
-        #         print(newMap[lookFor])
-        #         return [i, newMap[lookFor]]
-        #     else:
-        #         newMap[nums[i]] = i
