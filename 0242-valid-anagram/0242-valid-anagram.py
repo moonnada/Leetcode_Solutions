24e543cbf1b376c46004ec58b35dcac3f1d633c6
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-09 09:03:27 -0700

    Time: 37 ms (96.24%), Space: 16.9 MB (91.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,18 +2,17 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t): return False
         
-        dic = {}
+        d = {}
         
         for val in s:
-            if val in dic:
-                dic[val] += 1
+            if val in d:
+                d[val] += 1
             else:
-                dic[val] = 1
+                d[val] = 1
                 
         for val in t:
-            if val not in dic or dic[val] == 0:
-                return False
+            if not val in d or d[val] == 0: return False
             
-            dic[val] -= 1
+            d[val] -= 1
             
         return True
