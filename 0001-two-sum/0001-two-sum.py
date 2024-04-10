f2bff851b02fe3a048de03036162687895436092
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-10 08:33:26 -0700

    Time: 63 ms (51.98%), Space: 17.9 MB (16.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,6 +4,7 @@ class Solution:
         for i in range(len(nums)):
             lookFor = target - nums[i]
             if lookFor in d:
-                return [i, d[lookFor]]
+                return [ i , d[lookFor]]
             
-            d[nums[i]] = i        
+            d[nums[i]] = i
+            
