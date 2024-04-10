899a155053109ac93bad19d138cbb15803fea868
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-10 08:53:41 -0700

    Time: 84 ms (81.94%), Space: 19.3 MB (99.31%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,22 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        '''
-        1. check edge cases(empty or one char)
-        2. init a map(key: sorted word, value: a list of the sorted word)
-        3. traverse the input list to find group the anagram
-            3.1) sort the cur word
-            3.2) if the sorted word in the map, then add the original word into the map
-            3.3) else put the sorted word inthe map as key
-        4. return map.values
-        '''
-        
-        hmap = {}
-        
+        d = {}
         for word in strs:
             temp = ''.join(sorted(word))
-            if temp in hmap:
-                hmap[temp].append(word)
+            if temp in d:
+                d[temp].append(word)
+            
             else:
-                hmap[temp] = [word]
+                d[temp] = [word]
                 
-        return list(hmap.values())
+        return list(d.values())
