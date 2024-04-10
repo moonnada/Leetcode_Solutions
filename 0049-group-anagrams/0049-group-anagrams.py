d3ca3584f82181cf2bd6de0c71de7302b3ac070f
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-10 08:56:41 -0700

    Time: 85 ms (78.40%), Space: 19.5 MB (89.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,10 +2,10 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = {}
         for word in strs:
-            temp = ''.join(sorted(word))
+            temp = "".join(sorted(word))
             if temp in d:
                 d[temp].append(word)
-            
+                
             else:
                 d[temp] = [word]
                 
