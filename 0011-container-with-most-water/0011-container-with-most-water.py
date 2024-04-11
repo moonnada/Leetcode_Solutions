084856e2734af5b8d54be851a9a35dd567d39a88
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-10 18:06:53 -0700

    Time: 542 ms (20.73%), Space: 29.3 MB (88.92%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,35 +1,18 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        '''
-                         0 1 2 3 4 5 6 7 8
-        Input: height = [1,8,6,2,5,4,8,3,7]
-        Output: 49
+        left = 0
+        right = len(height) - 1
+        maxContainer = 0
         
-        key points: get valid width and height
-        
-        two pointers
-        
-        1. init two ptrs(left and right)
-        2. while left < right
-            2.1) get a height to compare left and right val(choose the longest one)
-            2.2) get a weight right - left + 1
-            2.3) if arr[left] < arr[right], left++
-            2.4) else right--
-            2.5) get the max container 
-        '''
-        
-        left, right = 0, len(height)-1
-        maxContain = 0
         while left < right:
             curHeight = min(height[left], height[right])
-            curWeight = right - left
+            curWeight = (right+1) - (left+1)
             
-            if height[left] < height[right]:
-                left += 1
-            else:
-                right -= 1
-
-                
-            maxContain = max(curHeight*curWeight, maxContain)
+            curContainer = curHeight * curWeight
+            maxContainer = max(maxContainer, curContainer)
             
-        return maxContain
+            if height[left] < height[right]: left +=1 
+            else: right -=1
+            
+        return maxContainer
+        
