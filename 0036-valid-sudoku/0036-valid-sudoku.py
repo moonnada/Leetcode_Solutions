32eaba998a3cc46871523291d02fc029301f9e4e
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-11 10:56:01 -0700

    Time: 86 ms (86.95%), Space: 16.7 MB (21.29%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -6,12 +6,31 @@ class Solution:
         
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".": continue
+                if board[r][c] ==".": continue
                     
-                elif board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in grid[r//3,c//3]: return False
+                elif board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in grid[r//3, c//3]:
+                    return False
                 
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
                 grid[r//3,c//3].add(board[r][c])
                 
         return True
+        
+        
+        
+#         rows = collections.defaultdict(set)
+#         cols = collections.defaultdict(set)
+#         grid = collections.defaultdict(set)
+        
+#         for r in range(9):
+#             for c in range(9):
+#                 if board[r][c] == ".": continue
+                    
+#                 elif board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in grid[r//3,c//3]: return False
+                
+#                 rows[r].add(board[r][c])
+#                 cols[c].add(board[r][c])
+#                 grid[r//3,c//3].add(board[r][c])
+                
+#         return True
