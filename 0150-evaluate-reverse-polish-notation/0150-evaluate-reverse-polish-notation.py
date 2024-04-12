da39b4c18542eec55943beb720f2efb3c60af01f
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-12 13:16:08 -0700

    Time: 62 ms (78.69%), Space: 17.1 MB (33.26%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -2,26 +2,49 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for i in tokens:
-            if i not in "+-*/":
+            if i not in  "+-*/":
                 stack.append(int(i))
-                continue
+            else:
+                num2 = int(stack.pop())
+                num1 = int(stack.pop())
+                match i:
+                    
+                    case "+":
+                        newVal = num1 + num2
+                        stack.append(newVal)
+                    case "-":
+                        newVal = num1 - num2
+                        stack.append(newVal)
+                    case "*":
+                        newVal = num1 * num2
+                        stack.append(newVal)
+                    case "/":
+                        newVal = int(num1 / num2)
+                        stack.append(newVal)
+        return stack.pop()
+    
+#         stack = []
+#         for i in tokens:
+#             if i not in "+-*/":
+#                 stack.append(int(i))
+#                 continue
             
          
             
-            num2 = stack.pop()
-            num1 = stack.pop()
+#             num2 = stack.pop()
+#             num1 = stack.pop()
             
-            if i == "+":
-                newVal = num1 + num2
-            if i == "-":
-                newVal = num1 - num2
-            if i == "*":
-                newVal = num1 * num2
-            if i == "/":
-                newVal = int(num1 / num2)
+#             if i == "+":
+#                 newVal = num1 + num2
+#             if i == "-":
+#                 newVal = num1 - num2
+#             if i == "*":
+#                 newVal = num1 * num2
+#             if i == "/":
+#                 newVal = int(num1 / num2)
                     
-            stack.append(newVal)
+#             stack.append(newVal)
                     
-        return stack.pop()
+#         return stack.pop()
                         
         
