3dd7e4e0da7f168ab0a548d601db3ef6cb2abb04
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-11 20:53:46 -0700

    Time: 51 ms (87.95%), Space: 16.8 MB (32.90%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,20 +1,42 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2): return False
-        countS1, countS2 = [0] * 26, [0]*26
+        countS1, countS2 = [0]*26, [0]*26
         left = 0
         
         for i in range(len(s1)):
             countS1[ord(s1[i]) - ord('a')] += 1
-            countS2[ord(s2[i]) - ord('a') ] += 1
+            countS2[ord(s2[i]) -ord('a')] += 1
             
         if countS1 == countS2: return True
         
-        for r in range(len(s1), len(s2)):
-            countS2[ord(s2[r]) - ord('a')] += 1
-            countS2[ord(s2[r - len(s1)]) - ord('a')] -= 1
+        for right in range(len(s1), len(s2)):
+            countS2[ord(s2[right]) - ord('a')] += 1
+            countS2[ord(s2[right - len(s1)]) - ord('a')] -= 1
             
             if countS1 == countS2: return True
-            
         return False
-         
+        
+#         s1d = s2d = {}
+        
+#         if len(s1) > len(s2): return False
+        
+#         for char in s1:
+#             s1d[char] = s1d.get(char, 0) + 1
+            
+        
+#         left = 0
+#         for right in range(len(s2)):
+#             s2d[s2[right]] = s2d.get(s2[right], 0)+1
+            
+#             while len(s1d) == len(s2d) and s1d != s2d:
+#                 s2d[s2[left]]-=1
+#                 if s2d[s2[left]] == 0:
+#                     del s2d[s2[left]]
+                
+#                 left += 1
+                
+#             if s1d == s2d: return True
+            
+#         return False
+        
