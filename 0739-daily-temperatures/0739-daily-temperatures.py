129ea8c1861a74369db9356d47e9ed83e87dee52
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-14 09:11:49 -0700

    Time: 887 ms (70.43%), Space: 32.1 MB (24.69%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,23 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        '''
-        Input: temperatures = [73,74,75,71,69,72,76,73]
-        Output: [1,1,4,2,1,1,0,0]
-        
-        1. init a stack and an ans list
-        2. iterative the input list from 1 to end 
-           
-        
-        '''
-        
         res = [0] * len(temperatures)
-        stack = []
+        stack = [] 
         
         for i,t in enumerate(temperatures):
             while stack and t > stack[-1][0]:
                 stackT, stackIdx = stack.pop()
                 res[stackIdx] = i - stackIdx
-            
+                
             stack.append([t,i])
-            
+        
         return res
