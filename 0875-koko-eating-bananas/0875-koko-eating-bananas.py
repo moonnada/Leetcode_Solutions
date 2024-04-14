f6781ae82fb8ad02327029c660c8e2b7c47597f2
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-04-14 14:19:37 -0700

    Time: 266 ms (62.69%), Space: 17.8 MB (99.73%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,14 +1,7 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        '''
-        
-                        1 2 2 3
-        Input: piles = [3,6,7,11], h = 8
-        Output: 4
-        
-        
-        '''
-        left, right = 1, max(piles)
+        left = 1
+        right = max(piles)
         
         while left <= right:
             mid = (left + right) // 2
@@ -19,6 +12,9 @@ class Solution:
             if hrs > h:
                 left = mid + 1
             else:
-                right = mid - 1
-                
+                right = mid- 1
+        
         return left
+                
+                
+        
