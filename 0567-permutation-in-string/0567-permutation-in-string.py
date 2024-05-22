2c6a84da6e01bcf429cf4684a1eb4c7badfc8b18
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-05-22 13:28:43 -0700

    Time: 50 ms (89.65%), Space: 16.8 MB (32.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,19 +1,33 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1d = {}
+        s2d = {}
+        left = 0
+        
         if len(s1) > len(s2): return False
         
-        cntS1 , cntS2 = [0]*26, [0]*26
+        for char in s1:
+            s1d[char] = s1d.get(char,0)+1
+            
         for i in range(len(s1)):
-            cntS1[ord(s1[i]) - ord('a')] += 1
-            cntS2[ord(s2[i]) - ord('a')] += 1
+            s2d[s2[i]] = s2d.get(s2[i],0) + 1
             
-        if cntS1 == cntS2: return True
-        
+        if s1d == s2d:
+            return True
         for right in range(len(s1), len(s2)):
-            cntS2[ord(s2[right]) - ord('a')] += 1
-            cntS2[ord(s2[right- len(s1)]) - ord('a')] -= 1
+            s2d[s2[right]] = s2d.get(s2[right], 0 ) + 1
             
-            if cntS1 == cntS2: return True
+            s2d[s2[left]] -= 1
+            if s2d[s2[left]] == 0:
+                del s2d[s2[left]]
+            left += 1
+            
+            if s1d == s2d: return True
             
         return False
+    
+    
+        
             
+            
+                
