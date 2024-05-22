9dc11c23246611e4a01106fbb353cb3852ad23d8
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-05-22 15:20:55 -0700

    Time: 35 ms (76.56%), Space: 16.4 MB (95.57%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,18 +5,6 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        '''
-        U:
-            q) list can have negative or decimal num?
-            
-        P: 
-            1. check edge cases(empty, one node)
-            2. init a prev ptr and ptr for both list
-            3. while each ptr.next exists
-                3.1) compare cur ptr and make a connection between small ptr. and then the ptr moves to the next one 
-            4. if one of list is null, then make a connection between the other list
-            
-        '''
         
         if not list1 and not list2: return None
         
@@ -24,8 +12,8 @@ class Solution:
         
         if not list2: return list1
         
-        prehead = ListNode(-1)
-        prev = prehead
+        preHead = ListNode(-1)
+        prev = preHead
         
         while list1 and list2:
             if list1.val <= list2.val:
@@ -36,7 +24,7 @@ class Solution:
                 list2 = list2.next
                 
             prev = prev.next
-                
-        prev.next = list1 if list1 is not None else list2
             
-        return prehead.next
+        prev.next = list1 if list1 is not None else list2
+        
+        return preHead.next
