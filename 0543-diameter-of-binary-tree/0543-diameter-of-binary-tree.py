f58b430e6a0e8548c1722ef1e61367df608e3884
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-05-29 15:52:18 -0700

    Time: 7286 ms (5.00%), Space: 19.2 MB (89.15%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -8,17 +8,43 @@ class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         if not root: return 0
         
-        self.diameter = 0
+        diameter = 0
+        que = [root]
         
-        def helper(node):
+        def getHeight(node):
             if not node: return 0
             
-            left = helper(node.left)
-            right = helper(node.right)
-            
-            self.diameter = max(left + right, self.diameter)
-            
-            return max(left, right)+1
+            return 1 + max(getHeight(node.left), getHeight(node.right))
         
-        helper(root)
-        return self.diameter
+        while que:
+            curNode = que.pop(0)
+            left = getHeight(curNode.left)
+            right = getHeight(curNode.right)
+            
+            if curNode.left:
+                que.append(curNode.left)
+            if curNode.right:
+                que.append(curNode.right)
+            
+            diameter = max(left+right, diameter)
+        
+        return diameter
+        
+        
+        #         time: o(n), space: o(1)
+#         if not root: return 0
+        
+#         self.diameter = 0
+        
+#         def helper(node):
+#             if not node: return 0
+            
+#             left = helper(node.left)
+#             right = helper(node.right)
+            
+#             self.diameter = max(left + right, self.diameter)
+            
+#             return max(left, right)+1
+        
+#         helper(root)
+#         return self.diameter
