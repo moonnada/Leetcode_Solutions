86e1a83c721c59854d48721d936cfed3b7cdf9ec
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-06-03 16:37:12 -0700

    Time: 28 ms (98.30%), Space: 17.1 MB (88.11%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,50 +6,26 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-#         BFS
-#         if not root: return []
-#         queue = [root]
-#         ans = []
-        
-#         while queue:
-#             curLevel = []
-            
-#             for i in range(len(queue)):
-#                 curNode = queue.pop(0)
-                
-#                 if curNode.left:
-#                     queue.append(curNode.left)
-                
-#                 if curNode.right:
-#                     queue.append(curNode.right)
-                    
-#                 curLevel.append(curNode.val)
-                
-#             ans.append(curLevel)
-            
-#         return ans
-        
-    
-#         DFS
         if not root: return []
-        
+        queue = [root]
         ans = []
         
-        def helper(node, level):
-            if not node: return
-
-            if len(ans) == level:
-                ans.append([])
-                
-            ans[level].append(node.val)
+        while queue:
+            curLevel = []
             
-            if node.left:
-                helper(node.left, level+1)
+            for i in range(len(queue)):
+                curNode = queue.pop(0)
                 
-            if node.right:
-                helper(node.right, level+1)
-        
-        helper(root, 0)
-
+                if curNode.left:
+                    queue.append(curNode.left)
+                
+                if curNode.right:
+                    queue.append(curNode.right)
+                    
+                curLevel.append(curNode.val)
+                
+            ans.append(curLevel)
+            
         return ans
-    
+        
+        
