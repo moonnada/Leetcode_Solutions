7aebe0f8eff307161b05c2f17698226d8ead2981
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-06-04 12:15:15 -0700

    Time: 177 ms (5.06%), Space: 30.8 MB (89.87%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,37 +6,20 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        '''
-        U:
-            key points: 
-                1. not bst
-                2. good node : in the path from root to cur node that are no nodes with a val greater than curnode   
-            q) input tree can be empty?
-            
-        M: DFS
-        
-        P:
-            1. check edge case(null) and init a count val
-            2. make a helper func to compare between child's vals and parents val
-                2.1) if not root, return 
-                2.2) if root.left exists, visit there and compare values. if it is a good node, cnt++
-                2.3) if root.right exists, visit there and compare values. if it is a good node, cnt++
-            3. return cnt
-        '''
         if not root: return 0
-        cnt = 0
+        good = 0
+        que = [[root, float(-inf)]]
         
-        def dfs(node, maxVal):
-            nonlocal cnt
-            if node.val >= maxVal: cnt+=1
+        while que:
+            [curNode, maxVal] = que.pop(0)
+            if not curNode: continue
+            if curNode.val >= maxVal:
+                maxVal = curNode.val
+                good += 1
+            
+            if curNode.left:
+                que.append([curNode.left, maxVal])
+            if curNode.right:
+                que.append([curNode.right, maxVal])
                 
-            if node.left:
-                dfs(node.left, max(maxVal, node.val))
-                
-            if node.right:
-                dfs(node.right, max(maxVal, node.val))
-        
-        dfs(root, float('-inf'))
-        
-        return cnt
-        
+        return good
