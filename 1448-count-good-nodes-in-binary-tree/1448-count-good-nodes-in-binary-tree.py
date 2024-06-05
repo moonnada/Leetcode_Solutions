1c001d1943c9f4558652b5c8168eaa62477095fd
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-06-05 09:26:43 -0700

    Time: 176 ms (5.09%), Space: 30.8 MB (89.84%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -7,39 +7,20 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         if not root: return 0
+        
+        que = [[root, float(-inf)]]
         ans = 0
         
-        def helper(node, maxVal):
-            nonlocal ans
+        while que:
+            [curNode, maxVal] = que.pop(0)
             
-            if node.val >= maxVal:
+            if curNode.val >= maxVal:
+                maxVal = curNode.val
                 ans += 1
                 
-            if node.left: helper(node.left, max(maxVal, node.val) )
-            if node.right: helper(node.right, max(maxVal, node.val))
-            
-        helper(root, float(-inf))
+            if curNode.left:
+                que.append([curNode.left, maxVal])
+            if curNode.right:
+                que.append([curNode.right, maxVal])
         
         return ans
-        
-        
-#         bfs. time: o(n), space: o(n)
-#         if not root: return 0
-#         que = [[root, float(-inf)]]
-#         good = 0
-        
-#         while que:
-#             [curNode, maxVal] = que.pop(0)
-            
-#             if curNode.val >= maxVal:
-#                 maxVal = curNode.val
-#                 good += 1
-                
-#             if curNode.left:
-#                 que.append([curNode.left, maxVal])
-            
-#             if curNode.right:
-#                 que.append([curNode.right, maxVal])
-                
-#         return good
-        
