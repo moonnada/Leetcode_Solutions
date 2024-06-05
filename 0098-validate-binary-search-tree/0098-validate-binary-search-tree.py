0d952375289d95c0804cf1550f4cdfa76c698413
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-06-05 10:01:47 -0700

    Time: 40 ms (66.56%), Space: 18.2 MB (78.28%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -7,15 +7,32 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         if not root: return False
+        que = [[root, float('-inf'), float('inf')]]
         
-        def helper(node, leftChild, rightChild ):
-            if not node: return True
+        while que:
+            [curNode, leftChild, rightChild]= que.pop(0)
             
-            elif node.val <= leftChild or node.val >= rightChild:
-                return False
+            if curNode.val <= leftChild or curNode.val >= rightChild: return False
             
-            else: return helper(node.left, leftChild, node.val) and helper(node.right, node.val, rightChild)
+            if curNode.left:
+                que.append([curNode.left, leftChild, curNode.val])
+            
+            if curNode.right:
+                que.append([curNode.right, curNode.val, rightChild])
+                
+        return True
+            
+#         dfs. time: o(n), space: o(n)
+#         if not root: return False
+        
+#         def helper(node, leftChild, rightChild ):
+#             if not node: return True
+            
+#             elif node.val <= leftChild or node.val >= rightChild:
+#                 return False
+            
+#             else: return helper(node.left, leftChild, node.val) and helper(node.right, node.val, rightChild)
             
         
-        return helper(root,float('-inf') ,float('inf') )
+#         return helper(root,float('-inf') ,float('inf') )
         
