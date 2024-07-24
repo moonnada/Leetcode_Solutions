e42f5f8d89662ea8cc2586254572cd40ce63f1e9
Ray Kim <53587841+moonnada@users.noreply.github.com>
2024-07-24 12:30:02 -0700

    Time: 42 ms (17.39%), Space: 16.4 MB (91.93%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,36 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        '''
-        U:
-        q) what should i return if both input arrs are empty
-        q) 
-        ex) 
-        s= "abc", t="ahbgdc"
-            L        R
-             L         R
-             
-        
-        M: two ptrs
-        
-        P: 
-        1. check edge cases(empty, compare each str length)
-        2. init a two ptrs for each str
-        3. traverse the longest arr to check there is subsequnece or not
-    
-        '''
-        
         if len(s) > len(t): return False
+        i, j = 0 ,0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
         
-        left, right = len(s), len(t)
-        p_left = 0
-        p_right = 0
-        while p_left < left and p_right < right:
-            if s[p_left] == t[p_right]:
-                p_left += 1
-            p_right += 1
-        
-        return p_left == left
-            
-           
-        
-        
+        return i == len(s)
